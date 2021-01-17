11f93dcd4b2b52f0ee3519334275cb03b73cea98
Ghislain HUDE <ghislain.hude@sap.com>
2021-01-17 20:30:05 +0100

    Add test check in the wasabi.sh
    Also remove useless sym. link, use abs. folder as possbile separate root folder and extrenal folder (which contains external bin. deps)

--- wasabi.sh
+++ wasabi.sh
@@ -8,36 +8,59 @@ warning() {
 
 
 export WASABI_ROOT=$(pwd)
+export WASABI_EXTERNAL=$(pwd)/external
 
-echo "Installing into " $WASABI_ROOT
+mkdir $WASABI_EXTERNAL
 
-
-echo -----------------------------------
-echo ---------- install clang ----------
-\rm -rf $WASABI_ROOT/llvm
-\rm -rf $WASABI_ROOT/clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04
-wget -qO - https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz | tar xfJ - 
-sleep 1
-mv $WASABI_ROOT/clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04 $WASABI_ROOT/llvm
 
 echo
 echo -----------------------------------
-echo --- install wasm librt in clang ---
-wget -qO - https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/libclang_rt.builtins-wasm32-wasi-12.0.tar.gz | tar xfz - -C $WASABI_ROOT/llvm/lib/clang/11.0.0
+echo " Installing external tools into " $WASABI_EXTERNAL
+
+
+echo
+echo -----------------------------------
+echo ---------- install clang ----------
+echo " $(date +"%T")"
+\rm -rf $WASABI_EXTERNAL/llvm
+\rm -rf $WASABI_EXTERNAL/clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04
+wget -qO - https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz | tar xfJ - -C $WASABI_EXTERNAL/ && mv $WASABI_EXTERNAL/clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04 $WASABI_EXTERNAL/llvm
+
+echo -----------------------------------
+echo ---- install clang wasm librt  ----
+echo " $(date +"%T")"
+wget -qO - https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/libclang_rt.builtins-wasm32-wasi-12.0.tar.gz | tar xfz - -C $WASABI_EXTERNAL/llvm/lib/clang/11.0.0
 
 echo
 echo -----------------------------------
 echo ------ install wasm sysroot  ------
-\rm -rf $WASABI_ROOT/wasi-sdk-12
-mkdir -p $WASABI_ROOT/wasi-sdk-12
-wget -qO - https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/wasi-sysroot-12.0.tar.gz | tar xfz - -C $WASABI_ROOT/wasi-sdk-12
+echo " $(date +"%T")"
+\rm -rf $WASABI_EXTERNAL/wasi-sdk-12
+mkdir -p $WASABI_EXTERNAL/wasi-sdk-12
+wget -qO - https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/wasi-sysroot-12.0.tar.gz | tar xfz - -C $WASABI_EXTERNAL/wasi-sdk-12
 
 echo
 echo -----------------------------------
 echo ------- install wasmtime runtime --
+echo " $(date +"%T")"
 # see packages on https://github.com/bytecodealliance/wasmtime/releases
 
-wget -qO - https://github.com/bytecodealliance/wasmtime/releases/download/v0.22.0/wasmtime-v0.22.0-x86_64-linux.tar.xz | tar xfJ -
-mv $WASABI_ROOT/wasmtime-v0.22.0-x86_64-linux $WASABI_ROOT/wasmtime-v0.22.0
+\rm -rf $WASABI_EXTERNAL/wasmtime-v0.22.0
+\rm -rf $WASABI_EXTERNAL/wasmtime-v0.22.0-x86_64-linux
+wget -qO - https://github.com/bytecodealliance/wasmtime/releases/download/v0.22.0/wasmtime-v0.22.0-x86_64-linux.tar.xz | tar xfJ -  -C $WASABI_EXTERNAL/ && mv $WASABI_EXTERNAL/wasmtime-v0.22.0-x86_64-linux $WASABI_EXTERNAL/wasmtime-v0.22.0
 
 
+echo
+echo -----------------------------------
+echo ---- test with wasmtime runtime ---
+echo " $(date +"%T")"
+\rm -rf $WASABI_ROOT/src/test/wasi/build
+mkdir $WASABI_ROOT/src/test/wasi/build
+cd $WASABI_ROOT/src/test/wasi/build
+cmake .. &> compile.log
+make >> compile.log
+$WASABI_EXTERNAL/wasmtime-v0.22.0/wasmtime ./MyExample
+
+
+
+echo "End $(date +"%T")"
