3b8e2cc0426210c3e5109a8d94e9079122781bf4
Clemence Chomel <clemence.chomel@sap.com>
2021-01-22 10:48:37 +0100

    Checking presence of python3, make and cmake

--- set_env.sh
+++ set_env.sh
@@ -16,3 +16,52 @@ export WASMTIME=$WASABI_EXTERNAL/wasmtime-v0.22.0
 export WASMTIME_LINUX=$WASMTIME-x86_64-linux
 
 export CJSON=$WASABI_EXTERNAL/cJSON
+
+export PYTHON=python3
+export MAKE=make
+export CMAKE=cmake
+
+echo -----------------------------------
+echo ----- check $PYTHON install  ------
+dpkg -s $PYTHON &> /dev/null 
+
+if [ $? -ne 0 ]
+
+        then
+            echo "$PYTHON not installed. Installing..."  
+            sudo apt-get update
+            sudo apt-get install $PYTHON
+
+        else
+            echo    "$PYTHON installed."
+    fi
+
+echo -----------------------------------
+echo ------ check $MAKE install  -------
+dpkg -s $MAKE &> /dev/null 
+
+if [ $? -ne 0 ]
+
+        then
+            echo "$MAKE not installed. Installing..."  
+            sudo apt-get update
+            sudo apt-get install $MAKE
+
+        else
+            echo    "$MAKE installed."
+    fi
+
+echo -----------------------------------
+echo ------ check $CMAKE install  ------
+dpkg -s $CMAKE &> /dev/null 
+
+if [ $? -ne 0 ]
+
+        then
+            echo "$CMAKE not installed. InstalLing..."  
+            sudo apt-get update
+            sudo apt-get install $CMAKE
+
+        else
+            echo    "$CMAKE installed."
+    fi
