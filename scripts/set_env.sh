47e90d851131ebfcada757b03c6d8acbd43c1844
Ghislain HUDE <ghislain.hude@sap.com>
2021-02-06 16:40:30 +0100

    verify WASABI_ROOT_DIR is set before use it

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-echo "root dir: " $WASABI_ROOT_DIR
+
+echo -----------------------------------
+echo --------- set variables  ----------
+if [ ! -d "$WASABI_ROOT_DIR" ];
+then
+echo "error: variable WASABI_ROOT_DIR not set";
+exit 1;
+else
+echo "WASABI_ROOT_DIR: " "'"$WASABI_ROOT_DIR"'";
+fi
+
+
 export WASABI_EXTERNAL_DIR=$WASABI_ROOT_DIR/external
+echo "WASABI_EXTERNAL_DIR: " "'"$WASABI_EXTERNAL_DIR"'";
 
 export LLVM_DIR=$WASABI_EXTERNAL_DIR/llvm
 export LLVM_AR=$LLVM_DIR/bin/llvm-ar
@@ -12,9 +24,13 @@ export CXX_COMPILER=$LLVM_DIR/bin/clang++
 
 export WASI_SDK_DIR=$WASABI_EXTERNAL_DIR/wasi-sdk-12
 export SYSROOT_DIR=$WASI_SDK_DIR/wasi-sysroot
+echo "SYSROOT_DIR: " "'"$SYSROOT_DIR"'";
 
 export WASMTIME_DIR=$WASABI_EXTERNAL_DIR/wasmtime-v0.22.0
 export WASMTIME_LINUX_DIR=$WASMTIME_DIR-x86_64-linux
 export WASMTIME=$WASMTIME_DIR/wasmtime
+echo "WASMTIME: " "'"$SYSROOT_DIR"'";
 
 export CJSON_DIR=$WASABI_EXTERNAL_DIR/cJSON
+
+echo
