1c06d72620ee7274543f2734c4c9d09090e442e0
Ghislain HUDE <ghislain.hude@sap.com>
2021-02-06 16:29:05 +0100

    remove sudo usage in script

--- scripts/check_env.sh
+++ scripts/check_env.sh
@@ -1,50 +1,43 @@
 #!/usr/bin/env bash
 
 export PYTHON=python3
-export MAKE=make
-export CMAKE=cmake
-
 echo -----------------------------------
 echo ----- check $PYTHON install  ------
 dpkg -s $PYTHON &> /dev/null 
-
 if [ $? -ne 0 ]
+	then
+		echo "Error: $PYTHON not installed. Please install it:"  
+		echo sudo apt-get update
+		echo sudo apt-get install $PYTHON
+		exit 1
+	else
+		echo    "$PYTHON installed."
+fi
 
-        then
-            echo "$PYTHON not installed. Installing..."  
-            sudo apt-get update
-            sudo apt-get install $PYTHON
-
-        else
-            echo    "$PYTHON installed."
-    fi
-
+export MAKE=make
 echo -----------------------------------
 echo ------ check $MAKE install  -------
 dpkg -s $MAKE &> /dev/null 
-
 if [ $? -ne 0 ]
+	then
+		echo "Error: $MAKE not installed. Please install it:"  
+		echo sudo apt-get update
+		echo sudo apt-get install $MAKE
+		exit 1
+	else
+		echo    "$MAKE installed."
+fi
 
-        then
-            echo "$MAKE not installed. Installing..."  
-            sudo apt-get update
-            sudo apt-get install $MAKE
-
-        else
-            echo    "$MAKE installed."
-    fi
-
+export CMAKE=cmake
 echo -----------------------------------
 echo ------ check $CMAKE install  ------
 dpkg -s $CMAKE &> /dev/null 
-
 if [ $? -ne 0 ]
-
-        then
-            echo "$CMAKE not installed. InstalLing..."  
-            sudo apt-get update
-            sudo apt-get install $CMAKE
-
-        else
-            echo    "$CMAKE installed."
-    fi
+	then
+		echo "Error: $CMAKE not installed. Please install it:"  
+		echo sudo apt-get update
+		echo sudo apt-get install $CMAKE
+		exit 1
+	else
+		echo    "$CMAKE installed."
+fi
