787341ad7d62cf269792219a473a97d094699d4c
Ghislain HUDE <ghislain.hude@sap.com>
2021-02-07 00:54:14 +0100

    Fix it

--- src/test/CPP/CPP_libcxx.hpp
+++ src/test/CPP/CPP_libcxx.hpp
@@ -1,6 +1,7 @@
 
-#define EXPORT __declspec(dllexport)
+#define EXPORT 
 
 EXPORT double d_STDSQRT_d(double d);
 EXPORT int* pi_NEW_INT_v();
 EXPORT void v_DELETE_pv(void* sz);
+EXPORT const char* str_VECTOR_DOUBLE_d(double d);
