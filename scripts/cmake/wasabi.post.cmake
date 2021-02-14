2dc941f0b478c1ea2466396a48a080a34bb42592
I056617 <ghislain.hude@sap.com>
2021-02-14 16:55:25 +0100

    add cmake post file to include to manage binaryes naming

---
+++ scripts/cmake/wasabi.post.cmake
@@ -0,0 +1,8 @@
+message (TRACE "--------------------------------------------------------------")
+message (TRACE "-- wasabi.post.cmake toolchain")
+
+set(CMAKE_EXECUTABLE_SUFFIX ".wasm")
+message(TRACE "- CMAKE_EXECUTABLE_SUFFIX='${CMAKE_EXECUTABLE_SUFFIX}'")
+
+
+message (TRACE "--------------------------------------------------------------")
