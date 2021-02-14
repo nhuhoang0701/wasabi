9db8e8c743df891a188875ef4c44e56c2bc4bdb9
Ghislain HUDE <ghislain.hude@sap.com>
2021-02-14 12:05:38 +0100

    Update wasabi.cmake

--- scripts/cmake/wasabi.cmake
+++ scripts/cmake/wasabi.cmake
@@ -1,8 +1,9 @@
-#**************************************************************
+message ("--------------------------------------------------------------")
+message ("-- wasabi.cmake toolchain")
 if(DEFINED WASMTIME)
-	message("WASMTIME='${WASMTIME}'")
+	message("- WASMTIME='${WASMTIME}'")
 elseif (DEFINED ENV{WASMTIME})
-	message("use WASMTIME=ENV'$ENV{WASMTIME}' instead WASMTIME='${WASMTIME}'")
+	message("- WASMTIME=ENV'$ENV{WASMTIME}' instead WASMTIME='${WASMTIME}'")
     set (WASMTIME "$ENV{WASMTIME}" CACHE PATH "project root" FORCE)
 else()
 	message(FATAL_ERROR "Missing 'WASMTIME' definition")
@@ -11,9 +12,9 @@ endif()
 
 #
 if(DEFINED SYSROOT_DIR)
-	message("SYSROOT_DIR='${SYSROOT_DIR}'")
+	message("- SYSROOT_DIR='${SYSROOT_DIR}'")
 elseif (DEFINED ENV{SYSROOT_DIR})
-	message("use SYSROOT_DIR=ENV'$ENV{SYSROOT_DIR}' instead SYSROOT_DIR='${SYSROOT_DIR}'")
+	message("- SYSROOT_DIR=ENV'$ENV{SYSROOT_DIR}' instead SYSROOT_DIR='${SYSROOT_DIR}'")
     set (SYSROOT_DIR "$ENV{SYSROOT_DIR}" CACHE PATH "project root" FORCE)
 else()
 	message(FATAL_ERROR "Missing 'SYSROOT_DIR' definition")
@@ -21,15 +22,14 @@ endif()
 
 #
 if(DEFINED LLVM_DIR)
-	message("LLVM_DIR='${LLVM_DIR}'")
+	message("- LLVM_DIR='${LLVM_DIR}'")
 elseif (DEFINED ENV{LLVM_DIR})
-	message("use LLVM_DIR=ENV'$ENV{LLVM_DIR}' instead LLVM_DIR='${LLVM_DIR}'")
+	message("- LLVM_DIR=ENV'$ENV{LLVM_DIR}' instead LLVM_DIR='${LLVM_DIR}'")
     set (LLVM_DIR "$ENV{LLVM_DIR}" CACHE PATH "project root" FORCE)
 else()
 	message(FATAL_ERROR "Missing 'LLVM_DIR' definition")
 endif()
 
-#**************************************************************
 
 set(CMAKE_SYSTEM_NAME Generic)
 
@@ -51,6 +51,7 @@ set(CMAKE_CXX_COMPILER_ID Clang)
 
 set(CMAKE_SYSROOT ${SYSROOT_DIR})
 set(CLANG_TARGET_TRIPLE wasm32-unknown-wasi)
+message("- CLANG_TARGET_TRIPLE='${CLANG_TARGET_TRIPLE}'")
 
 set(CMAKE_C_COMPILER ${LLVM_DIR}/bin/clang)
 set(CMAKE_C_COMPILER_TARGET ${CLANG_TARGET_TRIPLE})
@@ -62,3 +63,8 @@ set(CMAKE_AR ${LLVM_DIR}/bin/llvm-ar)
 set(CMAKE_SPLIT ${LLVM_DIR}/bin/llvm-split)	
 
 set(CMAKE_EXECUTABLE_SUFFIX ".wasm")
+
+set(CMAKE_BUILD_TYPE Debug)
+message("- CMAKE_BUILD_TYPE='${CMAKE_BUILD_TYPE}'")
+
+message ("--------------------------------------------------------------")
