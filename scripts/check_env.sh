2be92f59a34f97d65a1440205720e2cf26430ff1
PHAM Tung Nguyen <tung.nguyen.pham@sap.com>
2021-02-17 19:46:24 +0100

    Update check_env.sh

--- scripts/check_env.sh
+++ scripts/check_env.sh
@@ -55,7 +55,7 @@ Install_Steps()
 
 echo "Test OS version .... [$OS_RELEASE]"
 
-echo $OS_RELEASE | egrep "Microsoft|microsoft-standard" # > /dev/null 2>&1
+echo $OS_RELEASE | grep -E "Microsoft|microsoft-standard" # > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
 	# We are using WindowsSubsystemLinux
