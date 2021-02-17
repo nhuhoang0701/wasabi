1abae6e8e368ff8f7a5bbf2c787da2afca56bb97
I056617 <ghislain.hude@sap.com>
2021-02-17 01:58:26 +0100

    add cpp entry point for InA Interpreter

--- src/InA_Interpreter/main.cpp
+++ src/InA_Interpreter/main.cpp
@@ -1,6 +1,24 @@
-#include <iostream> 
+#include <iostream>
+
+#define WASM_EXPORT extern "C"
 
 int main()
 {
-  return 0;
+	std::cout << "main executed:"
+              << __FILE__ << ':'
+              << __LINE__ << '\n';
+	return 0;
+}
+
+WASM_EXPORT
+const char* json_getServerInfo()
+{
+	return "{\"hardcoded json returned from c++ json_getServerInfo\":1}";
+}
+
+WASM_EXPORT
+const char* json_getResponse_json(const char* InA)
+{
+	std::cout << "c++ json_getResponse_json: received from JS:" << InA << '\n';
+	return "{\"hardcoded json returned from c++ json_getResponse_json\":2}";
 }
