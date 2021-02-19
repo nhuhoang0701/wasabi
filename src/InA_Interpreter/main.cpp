a01b5b6d15c4385728f38404768a60f5cf6baafe
I056617 <ghislain.hude@sap.com>
2021-02-19 15:14:17 +0100

    using static std::string instead static char*

--- src/InA_Interpreter/main.cpp
+++ src/InA_Interpreter/main.cpp
@@ -16,9 +16,16 @@ const char* json_getServerInfo()
 	return "{\"hardcoded json returned from c++ json_getServerInfo\":1}";
 }
 
+std::string static_str;
 WASM_EXPORT
 const char* json_getResponse_json(const char* InA)
 {
 	std::cout << "c++ json_getResponse_json: received from JS:" << InA << '\n';
-	return "{\"hardcoded json returned from c++ json_getResponse_json\":2}";
+	static_str += "{";
+	static_str += "\"c++ received hardcoded json received from JS json_getResponse_json\":\"";
+	static_str += InA;
+	static_str += "\"";
+	static_str += "}";
+	
+	return static_str.c_str();
 }
