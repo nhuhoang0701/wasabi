0ccd291360be552a61d3c8c6ce008135035442a8
I056617 <ghislain.hude@sap.com>
2021-02-21 22:23:23 +0100

    cmake -> 3.19.5

--- scripts/wasabi_external.sh
+++ scripts/wasabi_external.sh
@@ -19,18 +19,17 @@ echo " Installing external tools into " $WASABI_EXTERNAL_DIR
 echo " Installing external tools into $WASABI_EXTERNAL_DIR" > $outfile
 
 
-
 echo
 echo -----------------------------------
 echo ---------- install cmake ----------
 echo " $(date +"%T")"
-export CMAKE_VERSION=${CMAKE_VERSION:-3.20.0}
-export CMAKEFile=cmake-$CMAKE_VERSION-rc1-linux-x86_64
+export CMAKE_VERSION=${CMAKE_VERSION:-3.19.5}
+export CMAKEFile=cmake-$CMAKE_VERSION-Linux-x86_64
 echo "CMAKE version: $CMAKE_VERSION"
 if [ ! -f "$WASABI_CMAKE_DIR/$CMAKEFile.flag" ]
 then
 	rm -rf $WASABI_EXTERNAL_DIR/$CMAKEFile
-	wget -qO - https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION-rc1/$CMAKEFile.tar.gz | tar xfz - -C $WASABI_EXTERNAL_DIR
+	wget -qO - https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/$CMAKEFile.tar.gz | tar xfz - -C $WASABI_EXTERNAL_DIR
 	
 	rm -rf $WASABI_CMAKE_DIR
 	mkdir -p $WASABI_CMAKE_DIR
