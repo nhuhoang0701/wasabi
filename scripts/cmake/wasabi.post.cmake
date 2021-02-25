42b7bf75f824954554bfde25962f6c352fb294a2
I056617 <ghislain.hude@sap.com>
2021-02-25 10:37:54 +0100

    remove .wasm suffi in cmake

--- scripts/cmake/wasabi.post.cmake
+++ scripts/cmake/wasabi.post.cmake
@@ -1,8 +1,5 @@
 message (TRACE "--------------------------------------------------------------")
 message (TRACE "-- wasabi.post.cmake toolchain")
 
-set(CMAKE_EXECUTABLE_SUFFIX ".wasm")
-message(TRACE "- CMAKE_EXECUTABLE_SUFFIX='${CMAKE_EXECUTABLE_SUFFIX}'")
-set(CMAKE_STATIC_LIBRARY_SUFFIX ".wasm.a")
 
 message (TRACE "--------------------------------------------------------------")
