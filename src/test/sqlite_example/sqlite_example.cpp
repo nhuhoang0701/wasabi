ca5b4887541d333813510f71cfbe2ac71c3cc7f1
I056617 <ghislain.hude@sap.com>
2021-02-25 01:00:07 +0100

    rename sqlite test to sqlite_example to vent conflict with sqlite, prepare external as cmake

---
+++ src/test/sqlite_example/sqlite_example.cpp
@@ -0,0 +1,76 @@
+#include <sqlite3.h>
+#include <stdio.h>
+
+int callback(void *, int, char **, char **);
+
+int main(void) {
+    
+    sqlite3 *db;
+    char *err_msg = 0;
+    
+    int rc = sqlite3_open(":memory:", &db);
+    
+    if (rc != SQLITE_OK) {
+        
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        
+        return 1;
+    }
+    
+    const char *sql = "CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT);"
+    "INSERT INTO Friends(Name) VALUES ('Tom');"
+    "INSERT INTO Friends(Name) VALUES ('Rebecca');"
+    "INSERT INTO Friends(Name) VALUES ('Jim');"
+    "INSERT INTO Friends(Name) VALUES ('Roger');"
+    "INSERT INTO Friends(Name) VALUES ('Robert');";
+        
+    
+    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    if (rc != SQLITE_OK ) {
+        
+        fprintf(stderr, "Failed to create table\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        
+    } else {
+        
+        fprintf(stdout, "Table Friends created successfully\n");
+    }
+    
+    int last_id = sqlite3_last_insert_rowid(db);
+    printf("The last Id of the inserted row is %d\n", last_id);
+    
+    rc = sqlite3_exec(db, "SELECT * FROM Friends", callback, 0, &err_msg);
+    if (rc != SQLITE_OK ) {
+        
+        fprintf(stderr, "Failed to execute SQL\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        
+    } else {
+        
+        fprintf(stdout, "SQL successfully executed \n");
+    }
+	
+    sqlite3_close(db);
+    
+    return 0;
+}
+
+
+int callback(void *NotUsed, int argc, char **argv, 
+                    char **azColName) {
+    
+    NotUsed = 0;
+    
+    for (int i = 0; i < argc; i++) {
+
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
+
