836bc037457bc84fd30be03d2132ea74908c44e4
markus ulke <markus.ulke@sap.com>
2021-02-25 18:32:19 +0100

    introduce metadata_enrichment

--- src/metadata_enrichment/test/ServerInfo_test.cpp
+++ src/metadata_enrichment/test/ServerInfo_test.cpp
@@ -1,10 +1,14 @@
 #include <sstream>
 #include "metadata_enrichment/ServerInfo.h"
 #include "json/jsonReader.h"
+#include <iostream>
+
+using namespace wasabi::metadata;
 void testServerInfo(){
   const ServerInfo aSI("en");
   std::stringstream aStream;
   aStream << aSI;
   JSONReader aReader;
   aReader.parse(aStream.str());
+  std::cout << "SystemInfo: " << std::endl << aStream.str() << std::endl;
 }
