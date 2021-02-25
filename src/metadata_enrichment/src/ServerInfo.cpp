91808f824f50cd3b09e84c82266dc34388765329
markus ulke <markus.ulke@sap.com>
2021-02-25 11:51:09 +0100

    ServerInfo
    Former-commit-id: c458ee0a95b5ca2099fda7c9036868f49588aa86

--- src/metadata_enrichment/src/ServerInfo.cpp
+++ src/metadata_enrichment/src/ServerInfo.cpp
@@ -1,4 +1,5 @@
 #include "metadata_enrichment/ServerInfo.h"
+#include "json/jsonWriter.h"
 namespace wasabi{
   namespace metadata{
     ServerInfo::ServerInfo(
@@ -6,7 +7,33 @@ namespace wasabi{
       ):itsLanguage(theLanguage),
         itsDBMS("HDB"),
         itsSystemId("WEB"),
-        itsReentranceTicket("<dummy>"){
+        itsReentranceTicket("<dummy>"),
+        itsClient("000"){
+    }
+    const string& ServerInfo::getClient()const{
+      return itsClient;
+    }
+    const string& ServerInfo::getDataBaseManagementSystem()const{
+      return itsDBMS;
+    }
+    const string& ServerInfo::getReentranceTicket()const{
+      return itsReentranceTicket;
+    }
+    const string& ServerInfo::getSystemId()const{
+      return itsSystemId;
+    }
+    const string& ServerInfo::getUserLanguageCode()const{
+      return itsSystemId;
+    }
+    ostream& operator<<(ostream& theStream, const ServerInfo& theServerInfo){
+      JSONWriter aWriter(theStream);
+      JSON_MAP(aWriter);
+      aWriter.pair("",theServerInfo.getClient());
+      aWriter.pair("",theServerInfo.getSystemId());
+      aWriter.pair("",theServerInfo.getDataBaseManagementSystem());
+      aWriter.pair("",theServerInfo.getUserLanguageCode());
+      aWriter.pair("",theServerInfo.getReentranceTicket());
+      return theStream;
     }
   }
 }
