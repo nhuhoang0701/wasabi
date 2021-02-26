93af56d9f9ee399c7a2c0d15b0cb2d5dc09c31a7
ahmad hajj sleiman <ahmad.hajj.sleiman@sap.com>
2021-02-26 16:00:23 +0100

    add test for POSIX write function

---
+++ src/test/C/C_write.c
@@ -0,0 +1,47 @@
+#include "test_tools/TestAssert.h"
+
+#include <stdio.h>         // For printf
+#include <math.h>          // For pow, sqrt
+#include <string.h>        // For memcpy
+#include <stdlib.h>        // For malloc and co
+
+//TODO: Centralyse it
+#define EXPORT 
+
+char buf[1024];
+
+EXPORT char* str_OPENFILE_WRITE_READ_str(const char* file)
+{
+	char out[10] = "coucou";
+
+	for(size_t i = 0; i < 1024; ++i)
+		buf[i] = 0;
+
+	FILE* fp = fopen (file, "w");
+	if(fp == 0)
+		return "Error during fopen call";
+	int res = fwrite(out, 1, 6, fp);
+	fclose(fp);
+
+	fp = fopen (file, "r");
+	if(fp == 0)
+		return "Error during fopen call";
+	res = fread(buf, 1, sizeof buf, fp);
+	if(res < 0)
+		return "Error during fread call";
+
+	if (ferror(fp))
+		return "Error during ferror call";
+
+	fclose(fp);
+
+	return buf;
+}
+
+int main()
+{
+   TEST_INIT();
+   CPPUNIT_ASSERT_EQUAL_STR(str_OPENFILE_WRITE_READ_str("../resources/textout.txt"), "coucou");
+
+   return TEST_HAVEERROR();
+}
