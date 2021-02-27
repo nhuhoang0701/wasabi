bf53242918c44187b21c71ddeeeea633d6370822
I056617 <ghislain.hude@sap.com>
2021-02-28 00:35:11 +0100

    [dbproxy] separate data and metadat tests
    Former-commit-id: 3842c168a8139ec60788321ece4bb634c01e9213

--- src/dbproxy/sqlite/sqlite.cpp
+++ src/dbproxy/sqlite/sqlite.cpp
@@ -7,14 +7,18 @@
 
 #include <iostream>
 
-
+const bool useDBFile = false;
 namespace dbproxy
 {
 	DBSQLite::DBSQLite()
 	: DBProxy()
 	{
-		if (sqlite3_open(":memory:", &m_sqlite_db) != SQLITE_OK)
-		//if (sqlite3_open_v2("../sqlite/efashion.db", &m_sqlite_db, SQLITE_OPEN_READONLY, nullptr) != SQLITE_OK)
+		int res = 0;
+		if(useDBFile)
+			res = sqlite3_open_v2("../sqlite/efashion.db", &m_sqlite_db, SQLITE_OPEN_READONLY, nullptr);
+		else
+			res = sqlite3_open(":memory:", &m_sqlite_db);
+		if (res != SQLITE_OK)
 		{
 			const std::string err = sqlite3_errmsg(m_sqlite_db);
 			std::cerr << "Cannot open database: '" << err << std::endl;
@@ -23,32 +27,35 @@ namespace dbproxy
 			throw std::runtime_error("Cannot open database:" + err);
 		}
 
-		//***********************************************************
-		// Create a DB on the fly with a SQL , waiting WASI read file
-		
-		// Create the tables
-		if (sqlite3_exec(m_sqlite_db, createFashionTablesSQL, 0, 0, &sqlite_err_msg) != SQLITE_OK )
+		if(useDBFile == false)
 		{
-			const std::string err_msg(sqlite_err_msg);
-			sqlite3_free(sqlite_err_msg);
-			std::cerr << "Failed to create tables, SQL error: '" << err_msg << "'" << std::endl;
-			//throw std::runtime_error("sqlite3_exec: " + std::string(err_msg));
-		}
-		else
-		{
-			std::cout << "Tables created successfully" << std::endl;
-		}
-		// insert data
-		if (sqlite3_exec(m_sqlite_db, eFashionInsertDataInto_table_Agg_yr_qt_mt_mn_wk_rg_cy_sn_sr_qt_ma, 0, 0, &sqlite_err_msg) != SQLITE_OK )
-		{
-			const std::string err_msg(sqlite_err_msg);
-			sqlite3_free(sqlite_err_msg);
-			std::cerr << "Failed to insert data, SQL error: '" << err_msg << "'" << std::endl;
-			//throw std::runtime_error("sqlite3_exec: " + std::string(err_msg));
-		}
-		else
-		{
-			std::cout << "Table eFashionInsertDataInto_table_Agg_yr_qt_mt_mn_wk_rg_cy_sn_sr_qt_ma data inserted successfully" << std::endl;
+			//***********************************************************
+			// Create a DB on the fly with a SQL , waiting WASI read file
+			
+			// Create the tables
+			if (sqlite3_exec(m_sqlite_db, createFashionTablesSQL, 0, 0, &sqlite_err_msg) != SQLITE_OK )
+			{
+				const std::string err_msg(sqlite_err_msg);
+				sqlite3_free(sqlite_err_msg);
+				std::cerr << "Failed to create tables, SQL error: '" << err_msg << "'" << std::endl;
+				//throw std::runtime_error("sqlite3_exec: " + std::string(err_msg));
+			}
+			else
+			{
+				std::cout << "Tables created successfully" << std::endl;
+			}
+			// insert data
+			if (sqlite3_exec(m_sqlite_db, eFashionInsertDataInto_table_Agg_yr_qt_mt_mn_wk_rg_cy_sn_sr_qt_ma, 0, 0, &sqlite_err_msg) != SQLITE_OK )
+			{
+				const std::string err_msg(sqlite_err_msg);
+				sqlite3_free(sqlite_err_msg);
+				std::cerr << "Failed to insert data, SQL error: '" << err_msg << "'" << std::endl;
+				//throw std::runtime_error("sqlite3_exec: " + std::string(err_msg));
+			}
+			else
+			{
+				std::cout << "Table eFashionInsertDataInto_table_Agg_yr_qt_mt_mn_wk_rg_cy_sn_sr_qt_ma data inserted successfully" << std::endl;
+			}
 		}
 
 		//***********************************************************
@@ -73,6 +80,24 @@ namespace dbproxy
 			executeSQL("PRAGMA table_info("+table.getName()+");", &lambda);
 			//std::cout << "<<*******************************************************" << std::endl << std::endl;
 		}
+		/*
+		{
+			sqlite3_stmt* stmt = nullptr;
+			int rc = sqlite3_prepare_v2(m_sqlite_db, "SELECT * FROM pragma_table_info('sometable');", -1, &stmt, 0);
+			printf("first query rc = %d\n", rc);
+			while( sqlite3_step(stmt)==SQLITE_ROW )
+			{
+				for(int i=0; i<sqlite3_column_count(stmt); i++)
+				{
+					if( i ) 
+						printf(",");
+					printf("%s", sqlite3_column_text(stmt,i));
+				}
+				printf("\n");
+			}
+			sqlite3_finalize(stmt);
+		}
+		*/
 	}
 
 	DBSQLite::~DBSQLite()
