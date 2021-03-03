5fb54a005e2ae171f48773a40c167e5d06f4e131
I056617 <ghislain.hude@sap.com>
2021-03-03 17:56:45 +0100

    use json file as response of getserverinfo and get metadata expand cube for the catalog
    Former-commit-id: 6b5d4edd026d16b86099f1fc2ed1db0549d51419

--- src/InA_Interpreter/InA_Interpreter.h
+++ src/InA_Interpreter/InA_Interpreter.h
@@ -10,7 +10,10 @@ const char* json_getServerInfo();
 WASM_EXPORT
 const char* json_getResponse_json(const char* InA);
 
-void processRequest(const JSONGenericObject& object, JSONWriter& writer);
-
-void processAnalyticsRequest(const JSONGenericObject& object, JSONWriter& writer);
+// Internals
+namespace ina_interpreter
+{
+	const char* processRequest(const JSONGenericObject& object, JSONWriter& writer);
+	void processAnalyticsRequest(const JSONGenericObject& object, JSONWriter& writer);
+}
 	
