b6518e554e5fc4f7507fed3c58b1b73a06c796e4
i051517 <christophe.lam.woon.sinon.sinn@sap.com>
2021-03-04 13:20:43 +0100

    InA_query_model: InA_queryFilter & InA_queryFilterOperator
    Former-commit-id: 4c087282e3c712bacef082f3da24efa83592db22

--- src/InA_query_model/InA_query_model.cpp
+++ src/InA_query_model/InA_query_model.cpp
@@ -8,5 +8,9 @@ namespace query_model
     {
         m_objs.push_back(dimension);
     }
+    const std::vector<InA_member>& InA_query_model::getVisibleMembers(const InA_dimension& dimension) const 
+    {
+        return dimension.getMembers();
+    }
 
 } //query_model
