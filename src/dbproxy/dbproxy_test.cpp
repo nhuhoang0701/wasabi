18d8a730d07916067ef3e53f1a36d30bdbaefc72
Clemence Chomel <clemence.chomel@sap.com>
2021-03-05 14:43:39 +0100

    New set of tests with a unique table for DB Proxy

--- src/dbproxy/dbproxy_test.cpp
+++ src/dbproxy/dbproxy_test.cpp
@@ -9,8 +9,8 @@ int main()
 	TEST_INIT();
 
 	// The string parameter is just as example
-	static const std::string cnxStr("local:sqlite:efashion.db");
-	static const std::string tableNameStr("Agg_yr_qt_mt_mn_wk_rg_cy_sn_sr_qt_ma");
+	static const std::string cnxStr("local:sqlite:onetable_datatype.db");
+	static const std::string tableNameStr("onetable_datatype");
 	
 	using namespace dbproxy;
     std::shared_ptr<DBProxy> dbProxy = DBProxy::getDBProxy(cnxStr);	
@@ -20,38 +20,32 @@ int main()
 		{
 			if(line == 0)
 			{
-				CPPUNIT_ASSERT_EQUAL_STR(row[0].getString().c_str(),"1");
-				CPPUNIT_ASSERT_EQUAL_STR(row[1].getString().c_str(),"2014");
-				CPPUNIT_ASSERT_EQUAL_STR(row[2].getString().c_str(),"3");
-				CPPUNIT_ASSERT_EQUAL_STR(row[10].getString().c_str(),"6937.6");
+				CPPUNIT_ASSERT_EQUAL_STR(row[0].getString().c_str(),"text1");
+				CPPUNIT_ASSERT_EQUAL_STR(row[1].getString().c_str(),"1.0");
 			}
-			else if(line == 1977)
+			else if(line == 2)
 			{
-				CPPUNIT_ASSERT_EQUAL_STR(row[0].getString().c_str(),"1978");
-				CPPUNIT_ASSERT_EQUAL_STR(row[1].getString().c_str(),"2017");
-				CPPUNIT_ASSERT_EQUAL_STR(row[2].getString().c_str(),"3");
-				CPPUNIT_ASSERT_EQUAL_STR(row[10].getString().c_str(),"7618.2");
+				CPPUNIT_ASSERT_EQUAL_STR(row[0].getString().c_str(),"text3");
+				CPPUNIT_ASSERT_EQUAL_STR(row[1].getString().c_str(),"3.0");
 			}
 			
 			line++;
 		};
 		dbProxy->executeSQL("SELECT * FROM " + tableNameStr, &lambda);
 		//std::cout << "line:" << line << std::endl;
-		CPPUNIT_ASSERT_EQUAL(line,3000);
+		CPPUNIT_ASSERT_EQUAL(line,3);
 	}
 	
 	{
 		size_t line = 0;
 		std::function<void(const Row&)> lambda = [&line](const Row& row)
 		{
-			CPPUNIT_ASSERT_EQUAL(row.size(), 2);
-			CPPUNIT_ASSERT_EQUAL_STR(row[0].getString().c_str(),"2014");
-			CPPUNIT_ASSERT_EQUAL(std::stod(row[1].getString()),12129067.6);
+			CPPUNIT_ASSERT_EQUAL(row.size(), 1);
 			line++;
 		};
-		dbProxy->executeSQL("SELECT Yr,sum(Sales_revenue)/3 FROM " + tableNameStr, &lambda);
+		dbProxy->executeSQL("SELECT text FROM " + tableNameStr, &lambda);
 		//std::cout << "line:" << line << std::endl;
-		CPPUNIT_ASSERT_EQUAL(line,1);
+		CPPUNIT_ASSERT_EQUAL(line,3);
 	}
 	return TEST_HAVEERROR();
 }
