1635bb692fc31134ad7a18ba7bda21a67356a7f0
Clemence Chomel <clemence.chomel@sap.com>
2021-03-05 14:43:39 +0100

    New set of tests with a unique table for DB Proxy
    Former-commit-id: 18d8a730d07916067ef3e53f1a36d30bdbaefc72

---
+++ src/dbproxy/sqlite/onetable_datatype/createtable.h
@@ -0,0 +1,4 @@
+#pragma once
+
+const char* createOneTableSQL =
+"CREATE TABLE onetable_datatype (text TEXT, real REAL);\n";
---
+++ src/dbproxy/sqlite/onetable_datatype/table_onetable_datatype.h
@@ -0,0 +1,5 @@
+#pragma once
+
+const char* data_onetable_datatype =
+"INSERT INTO onetable_datatype VALUES (\"text1\",\"1.0\"), (\"text2\",\"2.0\"), (\"text3\",\"3.0\");"
+;
