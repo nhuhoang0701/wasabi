57c26698afc24b0335093e7b748d509c121817ea
I056617 <ghislain.hude@sap.com>
2021-03-05 20:30:34 +0100

    [wasi/test] change some printf by some test assertion

--- src/test/C/C_clock_process.c
+++ src/test/C/C_clock_process.c
@@ -25,13 +25,11 @@ EXPORT void CLOCK_GET_TIME()
      clk_id = CLOCK_PROCESS_CPUTIME_ID;
 //
       // int clock_gettime(clockid_t clk_id, struct timespec *tp);
-      result = clock_gettime(clk_id, &tp);
-      printf("result: %i\n", result);
+      CPPUNIT_ASSERT_EQUAL(0, clock_gettime(clk_id, &tp));
       printf("tp.tv_sec: %lld\n", tp.tv_sec);
       printf("tp.tv_nsec: %ld\n", tp.tv_nsec);
 
-     result = clock_getres(clk_id, &tp);
-     printf("result: %i\n", result);
+     CPPUNIT_ASSERT_EQUAL(0, result = clock_getres(clk_id, &tp));
      printf("tp.tv_sec: %lld\n", tp.tv_sec);
      printf("tp.tv_nsec: %ld\n", tp.tv_nsec);
 
