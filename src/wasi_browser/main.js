557af68dfe40e45227bd381bf882bad1e1f11b3d
I056617 <ghislain.hude@sap.com>
2021-03-05 22:08:52 +0100

    [Microcube worker] Add ID the the worker message
    Former-commit-id: 1283d15752bd177110f7618709126f01f728d094

--- src/wasi_browser/main.js
+++ src/wasi_browser/main.js
@@ -1,19 +1,33 @@
 var worker = new Worker("Microcube_worker.js");
-worker.postMessage(["load"]);
+
+ID = 0;
+worker.postMessage([ID++, "load"]);
 
 worker.onmessage = function(msg) {
-	//alert("Receive from Microcube_worker: " + msg.data);
+	response_ID = msg.data[0];
+	response_Action = msg.data[1];
 	
-	if(msg.data[1] && msg.data[1].stack && msg.data[1].message) {
-	} else if(msg.data[0]=="load")
-		worker.postMessage(["GetServerInfo"]);
-	else if(msg.data[0]=="GetServerInfo")
-		worker.postMessage(["GetResponse", ["{\"Analytics\":{\"DataSource\":{\"CustomProperties\":{\"cnxString\":\"local:sqlite:efashion.db\"},"
+	console.log("worker.onmessage: " + msg.data);
+	console.log("worker.onmessage, received msg ID:" + response_ID);
+	
+	if(response_Action && response_Action.stack && response_Action.message)
+	{
+		console.error("worker.onmessage, response_ID:'" + response_ID + "' :" + response_Action.message);
+	}
+	else if(response_Action=="load")
+	{
+		worker.postMessage([ID++, "GetServerInfo"]);
+	}
+	else if(response_Action=="GetServerInfo")
+	{
+		worker.postMessage([ID++, "GetResponse", ["{\"Analytics\":{\"DataSource\":{\"CustomProperties\":{\"cnxString\":\"local:sqlite:efashion.db\"},"
 		+"\"ObjectName\":\"Agg_yr_qt_mt_mn_wk_rg_cy_sn_sr_qt_ma\",\"PackageName\":\"/\",\"Type\":\"sqlite\"},\"Definition\":{\"Description\":\"Order count\","
 		+"\"Dimensions\":[{\"Name\":\"Yr\",\"Axis\":\"Rows\"},{\"Name\":\"Qtr\",\"Axis\":\"Rows\"},{\"Name\":\"agg1_id\",\"Axis\":\"Rows\"}],"
 		+"\"DataSource\":{\"PackageName\":\"liquid-sqe\",\"ObjectName\":\"LIQUID_SALES_AV1\"},\"Name\":\"Query35\","
 		+"\"ReadMode\":\"BookedAndSpaceAndState\"}},\"Description\":\"Order count\",\"Name\":\"QMDS0035\","
 		+"\"TestNumber\":66}"]]);
-	else if(msg.data[0]=="GetResponse") {
+	}
+	else if(response_Action=="GetResponse")
+	{
 	}
 };
