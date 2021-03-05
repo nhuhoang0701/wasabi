4e138bf6b55a411f7a9831905c379044f755d77e
I056617 <ghislain.hude@sap.com>
2021-03-05 23:46:45 +0100

    [WASI] clean error and fixe missing return error code

--- src/wasi_browser/main.js
+++ src/wasi_browser/main.js
@@ -14,20 +14,29 @@ worker.onmessage = function(msg) {
 	{
 		console.error("worker.onmessage, response_ID:'" + response_ID + "' :" + response_Action.message);
 	}
-	else if(response_Action=="load")
+	else if(response_ID == 0 && response_Action=="load")
 	{
 		worker.postMessage([ID++, "GetServerInfo"]);
 	}
-	else if(response_Action=="GetServerInfo")
+	else if(response_ID == 1 && response_Action=="GetServerInfo")
 	{
-		worker.postMessage([ID++, "GetResponse", ["{\"Analytics\":{\"DataSource\":{\"CustomProperties\":{\"cnxString\":\"local:sqlite:efashion.db\"},"
+		worker.postMessage([ID++, "GetResponse", '{"Metadata":{"DataSource": {"ObjectName": "$$DataSource$$"}, "Expand":["Cubes"]}}']);
+	}
+	else if(response_ID == 2 && response_Action=="GetResponse")
+	{
+		worker.postMessage([ID++, "GetResponse", '{"Metadata":{"DataSource": {"ObjectName": "$$DataSource$$","PackageName":"local:sqlite:efashion.db"}, "Expand":["Cubes"]}}']);
+	}
+	else if(response_ID == 3 && response_Action=="GetResponse")
+	{
+		worker.postMessage([ID++, "GetResponse","{\"Analytics\":{\"DataSource\":{\"CustomProperties\":{\"cnxString\":\"local:sqlite:efashion.db\"},"
 		+"\"ObjectName\":\"Agg_yr_qt_mt_mn_wk_rg_cy_sn_sr_qt_ma\",\"PackageName\":\"/\",\"Type\":\"sqlite\"},\"Definition\":{\"Description\":\"Order count\","
 		+"\"Dimensions\":[{\"Name\":\"Yr\",\"Axis\":\"Rows\"},{\"Name\":\"Qtr\",\"Axis\":\"Rows\"},{\"Name\":\"agg1_id\",\"Axis\":\"Rows\"}],"
 		+"\"DataSource\":{\"PackageName\":\"liquid-sqe\",\"ObjectName\":\"LIQUID_SALES_AV1\"},\"Name\":\"Query35\","
 		+"\"ReadMode\":\"BookedAndSpaceAndState\"}},\"Description\":\"Order count\",\"Name\":\"QMDS0035\","
-		+"\"TestNumber\":66}"]]);
+		+"\"TestNumber\":66}"]);
 	}
-	else if(response_Action=="GetResponse")
+	else if(response_ID == 4 && response_Action=="GetResponse")
 	{
+		worker.postMessage([ID++, "GetServerInfo"]);
 	}
 };
