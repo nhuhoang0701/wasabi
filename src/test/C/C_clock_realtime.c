f0271574803a6249983e4048927ef1960c0acc2a
ahmad hajj sleiman <ahmad.hajj.sleiman@sap.com>
2021-03-05 13:58:50 +0100

    add clock tests
    Former-commit-id: e8af64398ba361fe1e638a688fac3b7f01cc70b6

---
+++ src/test/C/C_clock_realtime.c
@@ -0,0 +1,40 @@
+#include "test_tools/TestAssert.h"
+
+#include <stdio.h>
+#include <math.h>          // For pow, sqrt
+#include <string.h>        // For memcpy
+#include <stdlib.h>        // For malloc and co
+#include <unistd.h>
+#include <time.h>
+
+//TODO: Centralyse it
+#define EXPORT 
+
+
+EXPORT void CLOCK_GET_TIME()
+{
+
+    int result;
+    clockid_t clk_id;
+    struct timespec tp;
+
+
+  clk_id = CLOCK_REALTIME;
+//    clk_id = CLOCK_MONOTONIC;
+//    //  clk_id = CLOCK_BOOTTIME;
+//    //  clk_id = CLOCK_PROCESS_CPUTIME_ID;
+//
+      // int clock_gettime(clockid_t clk_id, struct timespec *tp);
+      result = clock_gettime(clk_id, &tp);
+      printf("result: %i\n", result);
+      printf("tp.tv_sec: %lld\n", tp.tv_sec);
+      printf("tp.tv_nsec: %ld\n", tp.tv_nsec);
+}
+
+int main()
+{
+   TEST_INIT();
+   CLOCK_GET_TIME();
+
+   return TEST_HAVEERROR();
+}
