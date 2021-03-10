cc7c2943d5e1b0cf644c35d9719bddb9a207da3a
I056617 <ghislain.hude@sap.com>
2021-03-10 21:12:17 +0100

    [wasabi browser]fix returned worker error gesture
    Former-commit-id: e36f71afdcfc8d2eea35cce2de62d3d3ed8dc0ff

--- src/wasi_browser/main.js
+++ src/wasi_browser/main.js
@@ -10,30 +10,33 @@ worker.onmessage = function(msg) {
 	
 	console.log("******************************************");
 	console.log("Worker: Message executed: ID='" + response_ID + "' Action='" + response_Action + "'");
-	console.log("Worker: Message executed: value='" + response_value.substring(0, 150) + "'");
 	
-	if(response_Action && response_Action.stack && response_Action.message)
-	{
-		console.error("worker.onmessage, response_ID:'" + response_ID + "' :" + response_Action.message);
+	if(response_value.stack && response_value.message)
+	{ 
+		console.error("worker.onmessage, response_ID:'" + response_ID + "' message:" + response_value.message);
 	}
-	else if(response_ID == 0 && response_Action=="load")
+	else
 	{
-		worker.postMessage([ID++, "GetServerInfo", ""]);
-	}
-	else if(response_ID == 1 && response_Action=="GetServerInfo")
-	{
-		worker.postMessage([ID++, "GetResponse", '{"Metadata":{"DataSource": {"ObjectName": "$$DataSource$$","PackageName":"local:sqlite:onetable_datatype.db"}, "Expand":["Cubes"]}}']);
-	}
-	else if(response_ID == 2 && response_Action=="GetResponse")
-	{
-		worker.postMessage([ID++, "GetResponse", '{"Metadata":{"DataSource": {"ObjectName": "onetable_datatype","PackageName":"local:sqlite:onetable_datatype.db"}, "Expand":["Cubes"]}}']);
-	}
-	else if(response_ID == 3 && response_Action=="GetResponse")
-	{
-		worker.postMessage([ID++, "GetResponse",'{"Analytics":{"DataSource":{"ObjectName":"onetable_datatype","PackageName":"local:sqlite:onetable_datatype.db","Type":"Wasabi"},"Definition":{"Dimensions":[{"Name":"text","Axis":"Rows"},{"Name":"varchar","Axis":"Rows"},{"Name":"CustomDimension1","Axis":"Cols","Members":[{"Description":"Measure 1","Name":"real", "Aggregation":"SUM"}]}]}}}']);
-	}
-	else if(response_ID == 4 && response_Action=="GetResponse")
-	{
-		worker.postMessage([ID++, "GetResponse",'{"Analytics":{"DataSource":{"ObjectName":"onetable_datatype","PackageName":"local:sqlite:onetable_datatype.db","Type":"Wasabi"},"Definition":{"Dimensions":[{"Name":"varchar","Axis":"Rows"},{"Name":"CustomDimension1","Axis":"Cols","Members":[{"Description":"Measure 1","Name":"real", "Aggregation":"SUM"},{"Description":"Measure 1","Name":"integer", "Aggregation":"SUM"}]}]}}}']);
+		console.log("Worker: Message executed: value='" + response_value.substring(0, 150) + "'");
+		if(response_ID == 0 && response_Action=="load")
+		{
+			worker.postMessage([ID++, "GetServerInfo", ""]);
+		}
+		else if(response_ID == 1 && response_Action=="GetServerInfo")
+		{
+			worker.postMessage([ID++, "GetResponse", '{"Metadata":{"DataSource": {"ObjectName": "$$DataSource$$","PackageName":"local:sqlite:efashion.db"}, "Expand":["Cubes"]}}']);
+		}
+		else if(response_ID == 2 && response_Action=="GetResponse")
+		{
+			worker.postMessage([ID++, "GetResponse", '{"Metadata":{"DataSource": {"ObjectName": "Agg_yr_qt_mt_mn_wk_rg_cy_sn_sr_qt_ma","PackageName":"local:sqlite:efashion.db"}, "Expand":["Cubes"]}}']);
+		}
+		else if(response_ID == 3 && response_Action=="GetResponse")
+		{
+			worker.postMessage([ID++, "GetResponse",'{"Analytics":{"DataSource":{"ObjectName":"Agg_yr_qt_mt_mn_wk_rg_cy_sn_sr_qt_ma","PackageName":"local:sqlite:efashion.db","Type":"Wasabi"},"Definition":{"Dimensions":[{"Name":"Yr","Axis":"Rows"},{"Name":"Month_name","Axis":"Rows"},{"Name":"CustomDimension1","Axis":"Cols","Members":[{"Description":"Measure 1","Name":"Sales_revenue", "Aggregation":"SUM"}]}]}}}']);
+		}
+		else if(response_ID == 4 && response_Action=="GetResponse")
+		{
+			worker.postMessage([ID++, "GetResponse",'{"Analytics":{"DataSource":{"ObjectName":"Agg_yr_qt_mt_mn_wk_rg_cy_sn_sr_qt_ma","PackageName":"local:sqlite:efashion.db","Type":"Wasabi"},"Definition":{"Dimensions":[{"Name":"Yr","Axis":"Rows"},{"Name":"CustomDimension1","Axis":"Cols","Members":[{"Description":"Measure 1","Name":"Sales_revenue", "Aggregation":"SUM"},{"Description":"Measure 1","Name":"Quantity_sold", "Aggregation":"SUM"}]}]}}}']);
+		}
 	}
 };
