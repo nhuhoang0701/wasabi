febb3ae00270f06b98a12e5a266e486c1047601b
I056617 <ghislain.hude@sap.com>
2021-03-12 01:19:41 +0100

    [calculator] test 4 storage
    Former-commit-id: 2e713ec4ff42156e74536545f897024c314f372f

--- src/calculator/storage.h
+++ src/calculator/storage.h
@@ -8,7 +8,6 @@
 #include <vector>
 #include <variant>
 
-
 namespace calculator
 {
 	typedef std::variant<std::string, double> Value;
@@ -18,6 +17,7 @@ namespace calculator
 
 	class ColumnNoneIndexed : public std::vector<Value>
 	{
+		public:
 		size_t getNbDistinctVals() const {return std::vector<Value>::size();}
 	};
 
@@ -27,15 +27,20 @@ namespace calculator
 		public:
 		size_t getNbDistinctVals() const {return m_values.size();}
 		size_t size() const {return std::vector<Index>::size();}
+		
+		const Value& operator[](size_t index) const {return m_values[std::vector<Index>::operator [](index)];}
 
 		void push_back(const Value& value)
 		{
 			Index index;
 			if(m_idxByVal.find(value) != m_idxByVal.end())
+			{
 				index = m_idxByVal[value];
+			}
 			else
 			{
 				index = m_values.size();
+				m_idxByVal[value] = index;
 				m_values.push_back(value);
 			}
 
@@ -69,17 +74,23 @@ namespace calculator
 
 		void    insertRow(const dbproxy::Row& row)
 		{
-			for(auto val : row)
-				for(auto col : m_cols)
-				{
-					const eColumnType type = std::get<1>(col);
-					if(type == eColumnType::Indexed)
-						std::get<ColumnIndexed>(std::get<2>(col)).push_back(val);
-					else if(type == eColumnType::NoneIndexed)
-						std::get<ColumnNoneIndexed>(std::get<2>(col)).push_back(val);
-					else
-						throw std::runtime_error("unknow column type");
-				}
+			if(row.size() != m_cols.size())
+				throw std::runtime_error("row size didn't match column size");
+
+			for(size_t i = 0; i < m_cols.size(); ++i)
+			{
+				auto& col = std::get<2>(m_cols[i]);
+				const eColumnType type = std::get<1>(m_cols[i]);
+				const eDataType dt = std::get<0>(m_cols[i]);
+
+				if(type == eColumnType::Indexed)
+					std::get<ColumnIndexed>(col).push_back(row[i]);
+				else if(type == eColumnType::NoneIndexed)
+					std::get<ColumnNoneIndexed>(col).push_back(std::stod(row[i]));
+				else
+					throw std::runtime_error("unknow column type");
+
+			}
 		}
 	};
 } // cube
--- src/calculator/storage_test.cpp
+++ src/calculator/storage_test.cpp
@@ -16,41 +16,62 @@ int main()
 	storage.addColumn(eDataType::String, eColumnType::Indexed);
 	CPPUNIT_ASSERT_EQUAL(1,storage.size());
 
-	storage.addColumn(eDataType::String, eColumnType::NoneIndexed);
+	storage.addColumn(eDataType::Number, eColumnType::NoneIndexed);
 	CPPUNIT_ASSERT_EQUAL(2,storage.size());
 
 	const auto& col0 = storage[0];
+	CPPUNIT_ASSERT_EQUAL(eDataType::String,std::get<0>(col0));
+	CPPUNIT_ASSERT_EQUAL(eColumnType::Indexed,std::get<1>(col0));
+
+	const auto& col1 = storage[1];
+	CPPUNIT_ASSERT_EQUAL(eDataType::Number,std::get<0>(col1));
+	CPPUNIT_ASSERT_EQUAL(eColumnType::NoneIndexed,std::get<1>(col1));
+
+	const auto& col0Data = std::get<ColumnIndexed>(std::get<2>(col0));
+	const auto& col1Data = std::get<ColumnNoneIndexed>(std::get<2>(col1));
 	{
 		dbproxy::Row row;
 		row.emplace_back("col0val0");
-		row.emplace_back("col1val0");
+		row.emplace_back("1.0");
 		storage.insertRow(row);
-		CPPUNIT_ASSERT_EQUAL(1,std::get<ColumnIndexed>(std::get<2>(col0)).getNbDistinctVals());
-		CPPUNIT_ASSERT_EQUAL(1,std::get<ColumnIndexed>(std::get<2>(col0)).size());
+		CPPUNIT_ASSERT_EQUAL(1,col0Data.getNbDistinctVals());
+		CPPUNIT_ASSERT_EQUAL(1,col1Data.getNbDistinctVals());
+		CPPUNIT_ASSERT_EQUAL(1,col0Data.size());
+		CPPUNIT_ASSERT_EQUAL(1,col1Data.size());
+		CPPUNIT_ASSERT_EQUAL("col0val0",std::get<std::string>(col0Data[0]));
+		CPPUNIT_ASSERT_EQUAL(1.0,std::get<double>(col1Data[0]));
 	}
 	{
 		dbproxy::Row row;
 		row.emplace_back("col0val1");
-		row.emplace_back("col1val1");
+		row.emplace_back("1.0");
 		storage.insertRow(row);
-		CPPUNIT_ASSERT_EQUAL(2,std::get<ColumnIndexed>(std::get<2>(col0)).getNbDistinctVals());
-		CPPUNIT_ASSERT_EQUAL(2,std::get<ColumnIndexed>(std::get<2>(col0)).size());
+		CPPUNIT_ASSERT_EQUAL(2,col0Data.getNbDistinctVals());
+		CPPUNIT_ASSERT_EQUAL(2,col1Data.getNbDistinctVals());
+		CPPUNIT_ASSERT_EQUAL(2,col0Data.size());
+		CPPUNIT_ASSERT_EQUAL(2,col1Data.size());
 	}
 	{
 		dbproxy::Row row;
 		row.emplace_back("col0val0");
-		row.emplace_back("col1val0");
+		row.emplace_back("2");
 		storage.insertRow(row);
-		CPPUNIT_ASSERT_EQUAL(2,std::get<ColumnIndexed>(std::get<2>(col0)).getNbDistinctVals());
-		CPPUNIT_ASSERT_EQUAL(3,std::get<ColumnIndexed>(std::get<2>(col0)).size());
+		CPPUNIT_ASSERT_EQUAL(2,col0Data.getNbDistinctVals());
+		CPPUNIT_ASSERT_EQUAL(3,col1Data.getNbDistinctVals());
+		CPPUNIT_ASSERT_EQUAL(3,col0Data.size());
+		CPPUNIT_ASSERT_EQUAL(3,col1Data.size());
 	}
 	{
 		dbproxy::Row row;
 		row.emplace_back("col0val1");
-		row.emplace_back("col1val1");
+		row.emplace_back("3.1");
 		storage.insertRow(row);
-		CPPUNIT_ASSERT_EQUAL(2,std::get<ColumnIndexed>(std::get<2>(col0)).getNbDistinctVals());
-		CPPUNIT_ASSERT_EQUAL(4,std::get<ColumnIndexed>(std::get<2>(col0)).size());
+		CPPUNIT_ASSERT_EQUAL(2,col0Data.getNbDistinctVals());
+		CPPUNIT_ASSERT_EQUAL(4,col1Data.getNbDistinctVals());
+		CPPUNIT_ASSERT_EQUAL(4,col0Data.size());
+		CPPUNIT_ASSERT_EQUAL(4,col1Data.size());
+		CPPUNIT_ASSERT_EQUAL("col0val1",std::get<std::string>(col0Data[3]));
+		CPPUNIT_ASSERT_EQUAL(3.1,std::get<double>(col1Data[3]));
 	}
 	
 	return TEST_HAVEERROR();
