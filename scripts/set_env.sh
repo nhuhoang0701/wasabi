9463ba34c5e6f8cd2c020b1f2ca08a1588bcbb9d
I056617 <ghislain.hude@sap.com>
2021-03-14 21:10:37 +0100

    setenv make only WASTIME  NINJA CMAKE exe variable mutabme
    Former-commit-id: fd84c2459900a2f6bd3117aedb7dde7cf323a830

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -14,35 +14,32 @@ export WASABI_EXTERNAL_DIR=$WASABI_ROOT_DIR/external
 echo -e "WASABI_EXTERNAL_DIR: " "\t'"$WASABI_EXTERNAL_DIR"'";
 
 export LLVM_DIR=${LLVM_DIR:-$WASABI_EXTERNAL_DIR/llvm}
+echo -e "LLVM_DIR: " "\t\t'"$LLVM_DIR"'";
 export LLVM_AR=$LLVM_DIR/bin/llvm-ar
 export LLVM_SPLIT=$LLVM_DIR/bin/llvm-split
-
 export C_COMPILER=$LLVM_DIR/bin/clang
-echo -e "C_COMPILER: " "\t\t'"$C_COMPILER"'";
-
 export CXX_COMPILER=$LLVM_DIR/bin/clang++
-echo -e "CXX_COMPILER: " "\t\t'"$CXX_COMPILER"'";
 
 export WASI_SDK_DIR=$WASABI_EXTERNAL_DIR/wasi-sdk-12
 export SYSROOT_DIR=$WASI_SDK_DIR/wasi-sysroot
 echo -e "SYSROOT_DIR: " "\t\t'"$SYSROOT_DIR"'";
 
-export WASMTIME_DIR=${WASMTIME_DIR:-$WASABI_EXTERNAL_DIR/wasmtime-v0.22.0}
+export WASMTIME_DIR=$WASABI_EXTERNAL_DIR/wasmtime-v0.22.0
 export WASMTIME_LINUX_DIR=$WASMTIME_DIR-x86_64-linux
-export WASMTIME=$WASMTIME_DIR/wasmtime
+export WASMTIME=${WASMTIME:-$WASMTIME_DIR/wasmtime}
 echo -e "WASMTIME: " "\t\t'"$WASMTIME"'";
 export WASMTIME_BACKTRACE_DETAILS=1
 
 export CJSON_DIR=$WASABI_EXTERNAL_DIR/cJSON
 export SQLITE_DIR=$WASABI_EXTERNAL_DIR/sqlite
 
-export WASABI_CMAKE_DIR=${WASABI_CMAKE_DIR:-$WASABI_EXTERNAL_DIR/cmake}
+export WASABI_CMAKE_DIR=$WASABI_EXTERNAL_DIR/cmake
 export CMAKE=${CMAKE:-$WASABI_CMAKE_DIR/bin/cmake}
 echo -e "CMAKE: "  "\t\t'"$CMAKE"'";
 export CTEST=${CTEST:-$WASABI_CMAKE_DIR/bin/ctest}
 echo -e "CTEST: "  "\t\t'"$CTEST"'";
 
-export WASABI_NINJA_DIR=${WASABI_NINJA_DIR:-$WASABI_EXTERNAL_DIR/ninja}
+export WASABI_NINJA_DIR=$WASABI_EXTERNAL_DIR/ninja
 export NINJA=${NINJA:-$WASABI_NINJA_DIR/ninja}
 echo -e "NINJA: "  "\t\t'"$NINJA"'";
 
@@ -67,7 +64,7 @@ alias setenv='unset WASABI_ROOT_DIR && source ./scripts/set_env.sh'
 alias run_wasabi_server='cd $WASABI_INSTAL_DIR; $HTTP_SERVER;cd -'
 alias clean='$CMAKE --build ./build --target clean'
 alias compile='$CMAKE --build ./build'
-alias test='(cd build && $CTEST -V)'
+alias test='(cd build && $CTEST -j8 -T test --output-on-failure)'
 alias install='$CMAKE --build ./build --target install'
 alias build='$CMAKE -B ./build . -G Ninja -DCMAKE_MAKE_PROGRAM=$NINJA -DCMAKE_INSTALL_PREFIX:PATH=$WASABI_INSTAL_DIR -DWASABI_USE_WASM=yes && install && test'
 alias rebuild='rm -rf ./build && mkdir build && build'
