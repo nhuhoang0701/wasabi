5dbfba729684ad37efe2ae3b61d6246766bc6dfc
I056617 <ghislain.hude@sap.com>
2021-03-18 16:13:33 +0100

    Clang compilation : CMAKE_EXE_LINKER_FLAGS="-static" . Add musl script compialtion
    Former-commit-id: 8c1013cfa600f987aafae111543a21372719d1e3 [formerly 8c1013cfa600f987aafae111543a21372719d1e3 [formerly 8c1013cfa600f987aafae111543a21372719d1e3 [formerly 8c1013cfa600f987aafae111543a21372719d1e3 [formerly 9867f60d167bcc517eb13d8378021d004eecb517]]]]
    Former-commit-id: 812a966bfecedf78a183e379f6a4508c7e9bb5ab
    Former-commit-id: 3a35dd8f9c31e9a397e70f5ef9357d888ccccb86
    Former-commit-id: eb144ecea5b9c5f8389fd4e77da0b8d4e50b922a
    Former-commit-id: 4950645a75e4bd0fb2521615abe21f280e6fad3c

--- scripts/clang_llvm.sh
+++ scripts/clang_llvm.sh
@@ -1,27 +1,16 @@
 
 
+echo
+echo -----------------------------------
+echo ---------- compile musl  ----------
 cd $WASABI_EXTERNAL_DIR
-rm -rf llvm4build || true
-mkdir -p llvm4build || true
-cd llvm4build
-git clone https://github.com/llvm/llvm-project.git
-cd llvm-project
-git checkout llvmorg-$LLVM_VERSION
-llvmorg-12.0.0-rc3
-rm -rf build || true
-mkdir build || true
-cd build
-$CMAKE  -G "Ninja" \
-        -DCMAKE_MAKE_PROGRAM=$NINJA \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_C_COMPILER=gcc \
-        -DCMAKE_C_FLAGS="-static" \
-        -DCMAKE_CXX_COMPILER=g++ \
-        -DCMAKE_CXX_FLAGS="-static-libstdc++" \
-        -DCMAKE_INSTALL_PREFIX=$LLVM_DIR \
-        -DLLVM_ENABLE_PROJECTS="clang;lld" \
-        -DLLVM_STATIC_LINK_CXX_STDLIB=ON \
-        -DLLVM_TARGETS_TO_BUILD="X86;WebAssembly" \
-        ../llvm
-$CMAKE --build . --target install
-touch $LLVM_DIR/$LLVMFile.flag
+mkdir musl4build
+cd musl4build
+git clone git://git.musl-libc.org/musl
+cd musl
+git checkout v1.2.2
+./configure --prefix=$WASABI_EXTERNAL_DIR/musl --syslibdir=--prefix=$WASABI_EXTERNAL_DIR/musl/syslib   CC=$C_COMPILER --disable-shared
+make -j4
+make install
+
+
