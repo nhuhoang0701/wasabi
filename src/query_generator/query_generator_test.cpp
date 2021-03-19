21001b09a7d62560d1e25ca7b55e0b37b46c1a15
Christophe LAM WOON SIN <christophe.lam.woon.sin@sap.com>
2021-03-19 15:09:45 +0100

    Filter on Members (#49)
    Co-authored-by: i051517 <christophe.lam.woon.sin on.sinn@sap.com>
    Former-commit-id: eff0b456084d9bf761f1970f084e542f94fcc88f [formerly eff0b456084d9bf761f1970f084e542f94fcc88f [formerly eff0b456084d9bf761f1970f084e542f94fcc88f [formerly eff0b456084d9bf761f1970f084e542f94fcc88f [formerly 638ba7869dac4edd50d084116c135161f263414d]]]]
    Former-commit-id: 44f1eef3079a166c51d3f57d95118667ab443be6
    Former-commit-id: 17d240c79c4ff42dbecf69d3b369288f2e810d8d
    Former-commit-id: 91ccf81d89a604f38bedec06e50ba02b78f0b536
    Former-commit-id: 58c73f2663d6121803ae7adca1f83acf3949b5a6

--- src/query_generator/query_generator_test.cpp
+++ src/query_generator/query_generator_test.cpp
@@ -2,37 +2,60 @@
 
 #include <InA_query_model/Query.h>
 #include <test_tools/TestAssert.h>
-
+#include <json/jsonReader.h>
 #include <iostream>
 
 int main()
 {
 	TEST_INIT();
 
-	ina::query_model::DataSource ds;
-	ds.setObjectName("MyTable");
+	{
+		ina::query_model::DataSource ds;
+		ds.setObjectName("MyTable");
 
-	ina::query_model::Query queryInA;
-	queryInA.setDataSource(ds);
-	
-	ina::query_model::Definition definition;
-	definition.addDimension(ina::query_model::Dimension("dim_A", ina::query_model::Dimension::eAxe::Rows));
-	
-	definition.addDimension(ina::query_model::Dimension("dim_B", ina::query_model::Dimension::eAxe::Rows));
-	
-	ina::query_model::Dimension dimensionMeasure("CustomDimension1", ina::query_model::Dimension::eAxe::Rows);
-		dimensionMeasure.addMember(ina::query_model::Member("meas_1", "SUM"));
-		dimensionMeasure.addMember(ina::query_model::Member("meas_2", "SUM"));
-	definition.addDimension(dimensionMeasure);
+		ina::query_model::Query queryInA;
+		queryInA.setDataSource(ds);
+		
+		ina::query_model::Definition definition;
+		definition.addDimension(ina::query_model::Dimension("dim_A", ina::query_model::Dimension::eAxe::Rows));
+		
+		definition.addDimension(ina::query_model::Dimension("dim_B", ina::query_model::Dimension::eAxe::Rows));
+		
+		ina::query_model::Dimension dimensionMeasure("CustomDimension1", ina::query_model::Dimension::eAxe::Rows);
+			dimensionMeasure.addMember(ina::query_model::Member("meas_1", "SUM"));
+			dimensionMeasure.addMember(ina::query_model::Member("meas_2", "SUM"));
+		definition.addDimension(dimensionMeasure);
 
-	queryInA.setDefinition(definition);
+		queryInA.setDefinition(definition);
 
-	const query_generator::query_generator& queryGen = query_generator::query_generator(queryInA);
-	std::string sql = queryGen.getSQL();
+		const query_generator::query_generator& queryGen = query_generator::query_generator(queryInA);
+		std::string sql = queryGen.getSQL();
 
-	std::cout << "Generated SQL: " << sql << std::endl;
+		std::cout << "Generated SQL: " << sql << std::endl;
 
-	CPPUNIT_ASSERT_EQUAL("SELECT dim_A, dim_B, SUM(meas_1), SUM(meas_2) FROM MyTable GROUP BY dim_A, dim_B;", sql);
-	
+		CPPUNIT_ASSERT_EQUAL("SELECT dim_A, dim_B, SUM(meas_1), SUM(meas_2) FROM MyTable GROUP BY dim_A, dim_B;", sql);
+	}
+	{
+		ina::query_model::DataSource ds;
+		ds.setObjectName("MyTable");
+
+		ina::query_model::Query queryInA;
+		queryInA.setDataSource(ds);
+
+		std::string request = R"({"Dimensions":[{"Attributes":[{"Name":"OBJ_188","Obtainability":"UserInterface"}],"Axis":"Rows","Name":"OBJ_188","NonEmpty":true,"ReadMode":"Booked","ResultStructure":[{"Result":"Members","Visibility":"Visible"}]},{"Attributes":[{"Name":"[Measures].[Measures]","Obtainability":"UserInterface"},{"Name":"[Measures].[Name]","Obtainability":"UserInterface"}],"Axis":"Columns","Members":[{"Aggregation":"SUM","MemberOperand":{"AttributeName":"Measures","Comparison":"=","Value":"OBJ_147"},"Visibility":"Visible"},{"Aggregation":"SUM","MemberOperand":{"AttributeName":"Measures","Comparison":"=","Value":"OBJ_262"},"Visibility":"Visible"}],"Name":"CustomDimension1","NonEmpty":false,"ReadMode":"Master"}],"DynamicFilter":{"Selection":{"Operator":{"Code":"And","SubSelections":[{"SetOperand":{"Elements":[{"Comparison":"=","Low":"OBJ_262"}],"FieldName":"[Measures].[Measures]"}},{"SetOperand":{"Elements":[{"Comparison":"=","Low":"2014"}],"FieldName":"OBJ_188"}}]}}},"ResultSetFeatureRequest":{"ResultEncoding":"None","ResultFormat":"Version2","ReturnedDataSelection":{"Actions":false,"CellDataType":false,"CellFormat":false,"CellMeasure":false,"CellValueTypes":false,"ExceptionAlertLevel":false,"ExceptionName":false,"ExceptionSettings":false,"Exceptions":false,"InputEnabled":false,"NumericRounding":false,"NumericShift":false,"TupleDisplayLevel":false,"TupleDrillState":false,"TupleElementIds":true,"TupleElementIndexes":false,"TupleLevel":false,"TupleParentIndexes":false,"UnitDescriptions":false,"UnitTypes":false,"Units":false,"Values":false,"ValuesFormatted":false,"ValuesRounded":false},"SubSetDescription":{"ColumnFrom":0,"ColumnTo":60,"RowFrom":0,"RowTo":500},"UseDefaultAttributeKey":false},"Sort":[]})";
+		JSONReader reader;
+		JSONGenericObject root = reader.parse(request);
+		ina::query_model::Definition definition;
+		read(definition, root);
+
+		queryInA.setDefinition(definition);
+
+		const query_generator::query_generator& queryGen = query_generator::query_generator(queryInA);
+		std::string sql = queryGen.getSQL();
+		CPPUNIT_ASSERT_EQUAL("SELECT OBJ_188, SUM(OBJ_262) FROM MyTable WHERE OBJ_188 = 2014 GROUP BY OBJ_188;", sql);
+		std::cout << "Generated SQL: " << sql << std::endl;
+	}
+
+
 	return TEST_HAVEERROR();
 }
