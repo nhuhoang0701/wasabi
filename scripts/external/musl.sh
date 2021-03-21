37d6b648a2d31e2bf340741c960edeb11f119069
I056617 <ghislain.hude@sap.com>
2021-03-21 21:39:31 +0100

    sysroot stuff
    Former-commit-id: dac13fc4559b2d8e058814c8a19237c2b9f4a290 [formerly dac13fc4559b2d8e058814c8a19237c2b9f4a290 [formerly d09241cce956004f9c2c38954965b5030ea109bd]] [formerly dac13fc4559b2d8e058814c8a19237c2b9f4a290 [formerly dac13fc4559b2d8e058814c8a19237c2b9f4a290 [formerly d09241cce956004f9c2c38954965b5030ea109bd]] [formerly dac13fc4559b2d8e058814c8a19237c2b9f4a290 [formerly d09241cce956004f9c2c38954965b5030ea109bd] [formerly d09241cce956004f9c2c38954965b5030ea109bd [formerly 1f390f70780d8e292f2f202f06d0d14a218f853c]]]]
    Former-commit-id: 9f0e45240b2f7a0fa00ace9515760684a31822c8
    Former-commit-id: 61521ee8ef315d0b26290e917fd2e9c918ee5e12
    Former-commit-id: 931ea4c8789679467684b2db8951ae1092529c7d
    Former-commit-id: 7ac70a29ef7d94d5fd65b06a5957e1a20d74dbdc

--- scripts/external/musl.sh
+++ scripts/external/musl.sh
@@ -12,7 +12,7 @@ then
     git clone git://git.musl-libc.org/musl
     cd musl
     git checkout v1.2.2
-    ./configure --prefix=$WASABI_EXTERNAL_DIR/musl --syslibdir=--prefix=$MUSL_DIR/syslib   CC=$C_COMPILER
+    ./configure --prefix=$WASABI_EXTERNAL_DIR/musl --syslibdir=--prefix=$SYSROOT_LINUX_DIR   CC=$C_COMPILER
     make -j$((`nproc`-2))
     make install
 
