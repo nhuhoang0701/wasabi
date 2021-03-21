d09241cce956004f9c2c38954965b5030ea109bd
I056617 <ghislain.hude@sap.com>
2021-03-21 21:39:31 +0100

    sysroot stuff
    Former-commit-id: 1f390f70780d8e292f2f202f06d0d14a218f853c

--- scripts/external/wasmsysroot.sh
+++ scripts/external/wasmsysroot.sh
@@ -1,23 +1,23 @@
 echo
 echo -----------------------------------
-echo ------ install wasm sysroot  ------
+echo ------ install wasi sysroot  ------
 echo "start at $(date +"%T")"
 export WASISDK_VERSION=${WASISDK_VERSION:-12}
 echo "WASISDK version: $WASISDK_VERSION"
-if [ ! -f "$WASI_SDK_DIR/wasisdk_$WASISDK_VERSION.flag" ]
+if [ ! -f "$SYSROOT_WASI_DIR/$WASISDK_VERSION.flag" ]
 then
-	mkdir -p $WASI_SDK_DIR
-	wget -qO - https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-$WASISDK_VERSION/wasi-sysroot-$WASISDK_VERSION.0.tar.gz | tar xfz - -C $WASI_SDK_DIR
-	touch $WASI_SDK_DIR/wasisdk_$WASISDK_VERSION.flag
+	mkdir -p $SYSROOT_DIR
+	wget -qO - https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-$WASISDK_VERSION/wasi-sysroot-$WASISDK_VERSION.0.tar.gz | tar xfz - -C $SYSROOT_DIR
+	touch $SYSROOT_WASI_DIR/$WASISDK_VERSION.flag
 else
-	echo "sysroot already installed in '$WASI_SDK_DIR'"
+	echo "already installed in '$SYSROOT_WASI_DIR'"
 fi
-if [ ! -f "$LLVM_DIR/lib/clang/$LLVM_VERSION/wasisdk_librt_$WASISDK_VERSION.flag" ]
+if [ ! -f "$LLVM_DIR/lib/clang/$LLVM_VERSION/lib/wasi/$WASISDK_VERSION.flag" ]
 then
 	mkdir -p $WASABI_EXTERNAL_DIR/llvm/lib/clang/$LLVM_VERSION
 	wget -qO - https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-$WASISDK_VERSION/libclang_rt.builtins-wasm32-wasi-$WASISDK_VERSION.0.tar.gz | tar xfz - -C $WASABI_EXTERNAL_DIR/llvm/lib/clang/$LLVM_VERSION
-	touch $LLVM_DIR/lib/clang/$LLVM_VERSION/wasisdk_librt_$WASISDK_VERSION.flag
+	touch $LLVM_DIR/lib/clang/$LLVM_VERSION/lib/wasi/$WASISDK_VERSION.flag
 else
-	echo "already installed in '$LLVM_DIR/lib/clang/$LLVM_VERSION'"
+	echo "already installed in '$LLVM_DIR/lib/clang/$LLVM_VERSION/lib/wasi'"
 fi
 echo "end at $(date +"%T")"
