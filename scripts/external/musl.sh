4c63517eb29627ebf2d29497e45a44a163c8ad08
I056617 <ghislain.hude@sap.com>
2021-03-21 22:01:54 +0100

    musl/sysroot
    Former-commit-id: 9bbcd62cf91e891c6fc327e34cf5d0a647595446 [formerly 9bbcd62cf91e891c6fc327e34cf5d0a647595446 [formerly 9bbcd62cf91e891c6fc327e34cf5d0a647595446 [formerly 9bbcd62cf91e891c6fc327e34cf5d0a647595446 [formerly db309b7e5a2cbb3ff1d6b14f85e5a435ea6013ee]]]]
    Former-commit-id: 0f6af490b8dd9ed4ba875e135df5bce449f85c4b
    Former-commit-id: 7636df5676a060e5bd27063fd751ed20babdab3f
    Former-commit-id: 319d9ef50aff01d1a7a0f2a06b28f227744f5d3e
    Former-commit-id: 02b67943f0098ccaefedc633f5512e44e6afd8d1

--- scripts/external/musl.sh
+++ scripts/external/musl.sh
@@ -4,7 +4,7 @@ echo
 echo -----------------------------------
 echo ---------- compile musl  ----------
 echo "start at $(date +"%T")"
-if [ ! -f "$MUSL_DIR/musl.flag" ]
+if [ ! -f "$SYSROOT_LINUX_DIR/musl.flag" ]
 then
     cd $WASABI_EXTERNAL_DIR
     mkdir -p musl4build
@@ -12,11 +12,11 @@ then
     git clone git://git.musl-libc.org/musl
     cd musl
     git checkout v1.2.2
-    ./configure --prefix=$WASABI_EXTERNAL_DIR/musl --syslibdir=--prefix=$SYSROOT_LINUX_DIR   CC=$C_COMPILER
+    ./configure --prefix=$SYSROOT_LINUX_DIR --syslibdir=--prefix=$SYSROOT_LINUX_DIR   CC=$C_COMPILER
     make -j$((`nproc`-2))
     make install
 
-	touch $MUSL_DIR/musl.flag
+	touch $SYSROOT_LINUX_DIR/musl.flag
 else
 	echo "already installed in '$MUSL_DIR'"
 fi
