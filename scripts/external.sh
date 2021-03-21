6b08364393d126cf7d7b08135a004e9692d39848
I056617 <ghislain.hude@sap.com>
2021-03-21 18:59:53 +0100

    spit big external.sh in several sh, to don't ahve a hugue one for LLVM toolschain preparation
    Former-commit-id: 3f5ba5d5c449d34ad0590ea3dcbb5fd54cf138e7 [formerly 3f5ba5d5c449d34ad0590ea3dcbb5fd54cf138e7 [formerly 436cc682f04e02fdfb3f8d47cbab1e925774b44c]]
    Former-commit-id: f39019c4d2c413d2ba93e708a9d39f9a1d7a7b15
    Former-commit-id: 29bcdba4e09fb1665bbbd37244a20491dab2d5d1

---
+++ scripts/external.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# exit on error
+set -e
+
+warning() {
+    printf '\n\033[1;33mWarning\033[0m: %s\n\n' "$1" 
+}
+
+source ./scripts/set_env.sh
+
+mkdir -p $WASABI_EXTERNAL_DIR
+
+export outfile=$WASABI_EXTERNAL_DIR/output_ext.log
+
+echo
+echo -----------------------------------
+echo " Installing external tools into '$WASABI_EXTERNAL_DIR'"
+echo "     log of this script will be in : '$outfile'"
+echo " Installing external tools into $WASABI_EXTERNAL_DIR" > $outfile
+
+
+$WASABI_ROOT_DIR/scripts/external/cmake.sh | tee -a $outfile
+$WASABI_ROOT_DIR/scripts/external/ninja.sh | tee -a $outfile
+export LLVM_VERSION=11.0.0
+$WASABI_ROOT_DIR/scripts/external/clang_llvm.sh | tee -a $outfile
+if [ "$WASABI_TOOLCHAIN" = "yes" ]
+then
+	echo
+	echo -----------------------------------
+	echo -------- compile toolhain ---------
+	$WASABI_ROOT_DIR/scripts/external/musl.sh | tee -a $outfile
+fi
+$WASABI_ROOT_DIR/scripts/external/wasmtime.sh | tee -a $outfile
+$WASABI_ROOT_DIR/scripts/external/wasmsysroot.sh | tee -a $outfile
+
+
+$WASABI_ROOT_DIR/scripts/external/sqlite.sh | tee -a $outfile
+$WASABI_ROOT_DIR/scripts/external/cjson.sh | tee -a $outfile
+
+
+echo "End $(date +"%T")"
---
+++ scripts/external/clang_llvm.sh
@@ -0,0 +1,80 @@
+echo 
+echo
+echo -----------------------------------
+echo ---------- install clang ----------
+echo " $(date +"%T")"
+echo "LLVM version: $LLVM_VERSION"
+export LLVM_ARCH=${LLVM_ARCH:-x86_64}
+export LLVM_OS=${LLVM_OS:-linux-gnu-ubuntu-20.04}
+export LLVMFile=clang+llvm-$LLVM_VERSION-$LLVM_ARCH-$LLVM_OS
+echo "LLVM version: $LLVMFile"
+echo "LLVM WASABI_LLVM: $WASABI_LLVM"
+if [ ! -f "$LLVM_DIR/$LLVMFile.$WASABI_LLVM.flag" ]
+then
+	if [ "$WASABI_LLVM" = "external" ]
+	then
+		rm -rf $LLVM_DIR
+		rm -rf $WASABI_EXTERNAL_DIR/$LLVMFile
+		wget -qO - https://github.com/llvm/llvm-project/releases/download/llvmorg-$LLVM_VERSION/$LLVMFile.tar.xz | tar xfJ - -C $WASABI_EXTERNAL_DIR/ && \rm -rf $LLVM_DIR && cp -rf $WASABI_EXTERNAL_DIR/$LLVMFile $LLVM_DIR
+		rm -rf $WASABI_EXTERNAL_DIR/$LLVMFile
+		touch $LLVM_DIR/$LLVMFile.$WASABI_LLVM.flag
+	elif [ "$WASABI_LLVM" = "compiled" ]
+	then
+		#source clang_llvm.sh
+		cd $WASABI_EXTERNAL_DIR
+		#rm -rf llvm4build || true
+		#mkdir -p llvm4build
+		cd llvm4build
+		#git clone https://github.com/llvm/llvm-project.git
+		cd llvm-project
+		git checkout llvmorg-$LLVM_VERSION
+		#rm -rf build || true
+		mkdir -p build
+		cd build
+		$CMAKE  -G "Ninja" \
+				-DCMAKE_MAKE_PROGRAM=$NINJA \
+				-DCMAKE_BUILD_PARALLEL_LEVEL=11 \
+				-DCMAKE_BUILD_TYPE=Release \
+				-DCMAKE_C_COMPILER=gcc \
+				-DCMAKE_EXE_LINKER_FLAGS="-static" \
+				-DCMAKE_C_FLAGS="-static" \
+				-DCMAKE_CXX_COMPILER=g++ \
+				-DCMAKE_CXX_FLAGS="-static-libstdc++" \
+				-DCMAKE_INSTALL_PREFIX=$LLVM_DIR \
+				-DLLVM_ENABLE_PROJECTS="lld" \
+				-DBUILD_SHARED_LIBS=OFF \
+				-DLLVM_STATIC_LINK_CXX_STDLIB=ON \
+				-DLLVM_TARGETS_TO_BUILD="X86;WebAssembly" \
+				-DLLVM_BUILD_TESTS=OFF \
+				-DLLVM_INCLUDE_TESTS=OFF \
+				-DCLANG_ENABLE_ARCMT=OFF \
+				../llvm >> $outfile
+		$CMAKE --build . --target install >> $outfile
+		touch $LLVM_DIR/$LLVMFile.$WASABI_LLVM.flag
+	elif [ "$WASABI_LLVM" = "git" ]
+	then
+		cd $WASABI_EXTERNAL_DIR
+		rm -rf $LLVM_DIR
+		tar --extract --file=llvm-$LLVM_VERSION.tar.gz
+		touch $LLVM_DIR/$LLVMFile.$WASABI_LLVM.flag
+	elif [ "$WASABI_LLVM" = "local" ]
+	then
+		echo "LLVM will be used from: '$LLVM_DIR'" 
+	else
+		echo "WASABI_LLVM should be one of {compiled|external|git|local} not '$WASABI_LLVM'"
+		return 1;
+	fi
+else
+	echo "Clang already installed in '$LLVM_DIR'"
+fi
+echo -----------------------------------
+echo "clang version:"
+$LLVM_DIR/bin/clang --version || true
+echo --------------
+ldd $LLVM_DIR/bin/clang || true
+echo -----------------------------------
+echo "lld version:"
+$LLVM_DIR/bin/lld --version || true
+echo --------------
+ldd $LLVM_DIR/bin/lld || true
+echo -----------------------------------
