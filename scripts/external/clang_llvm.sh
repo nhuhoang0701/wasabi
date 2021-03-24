63866148cc80752ba6bea4c2fd5a6ce348bfd1f3
I056617 <ghislain.hude@sap.com>
2021-03-24 23:10:48 +0100

    [llvm] force llvm cmpilation
    Former-commit-id: e46dc44dfd15f4df09d8866888839aa1307bb474 [formerly e46dc44dfd15f4df09d8866888839aa1307bb474 [formerly d3b2306de39d6b038f7674b7a56e58162cb4deac]]
    Former-commit-id: ea16f024158ca9b961d9646cc18cce44e5adcf8a
    Former-commit-id: 81553dff8cd3475355f56af07b98bcca5c415ec3

--- scripts/external/clang_llvm.sh
+++ scripts/external/clang_llvm.sh
@@ -9,7 +9,41 @@ export LLVM_OS=${LLVM_OS:-linux-gnu-ubuntu-20.04}
 export LLVMFile=clang+llvm-$LLVM_VERSION-$LLVM_ARCH-$LLVM_OS
 echo "LLVM version: $LLVMFile"
 echo "LLVM WASABI_LLVM: $WASABI_LLVM"
-if [ ! -f "$LLVM_DIR/$LLVMFile.$WASABI_LLVM.flag" ]
+
+if [ "$WASABI_LLVM" = "compiled" ]
+then
+	cd $WASABI_EXTERNAL_DIR
+	mkdir -p llvm4build
+	cd llvm4build
+	git clone https://github.com/llvm/llvm-project.git
+	cd llvm-project
+	git checkout llvmorg-$LLVM_VERSION
+	mkdir -p build
+	cd build
+	$CMAKE  -G "Ninja" \
+			-DCMAKE_MAKE_PROGRAM=$NINJA \
+			-DCMAKE_BUILD_PARALLEL_LEVEL=$((`nproc`-2)) \
+			-DCMAKE_BUILD_TYPE=Release \
+			-DCMAKE_C_COMPILER="${LLVM_DIR}/bin/clang" \
+			-DCMAKE_CXX_COMPILER="${LLVM_DIR}/bin/clang++" \
+			-DCMAKE_LINKER="${LLVM_DIR}/bin/clang" \
+			\
+			-DCMAKE_CXX_FLAGS="-stdlib=libc++  -I${LLVM_DIR}/include/c++/v1" \
+			\
+			-DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld -static -stdlib=libc++  -rtlib=compiler-rt -L${LLVM_DIR}/lib  -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libunwind.a -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libc++abi.a" \
+			-DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld -static -stdlib=libc++  -rtlib=compiler-rt -L${LLVM_DIR}/lib -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libunwind.a -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libc++abi.a" \
+			\
+			-DCMAKE_INSTALL_PREFIX=$LLVM_DIR \
+			-DBUILD_SHARED_LIBS=OFF \
+			-DLLVM_STATIC_LINK_CXX_STDLIB=ON \
+			-DLLVM_TARGETS_TO_BUILD="X86;WebAssembly" \
+			-DLLVM_BUILD_TESTS=OFF \
+			-DLLVM_INCLUDE_TESTS=OFF \
+			-DCLANG_ENABLE_ARCMT=OFF \
+			../llvm >> $outfile
+	$CMAKE --build . --target install >> $outfile
+	touch $LLVM_DIR/$LLVMFile.$WASABI_LLVM.flag
+elif [ ! -f "$LLVM_DIR/$LLVMFile.$WASABI_LLVM.flag" ]
 then
 	if [ "$WASABI_LLVM" = "external" ]
 	then
@@ -17,39 +51,6 @@ then
 		rm -rf $WASABI_EXTERNAL_DIR/$LLVMFile
 		wget -qO - https://github.com/llvm/llvm-project/releases/download/llvmorg-$LLVM_VERSION/$LLVMFile.tar.xz | tar xfJ - -C $WASABI_EXTERNAL_DIR/ && \rm -rf $LLVM_DIR && cp -rf $WASABI_EXTERNAL_DIR/$LLVMFile $LLVM_DIR
 		rm -rf $WASABI_EXTERNAL_DIR/$LLVMFile
-		touch $LLVM_DIR/$LLVMFile.$WASABI_LLVM.flag
-	elif [ "$WASABI_LLVM" = "compiled" ]
-	then
-		cd $WASABI_EXTERNAL_DIR
-		mkdir -p llvm4build
-		cd llvm4build
-		git clone https://github.com/llvm/llvm-project.git
-		cd llvm-project
-		git checkout llvmorg-$LLVM_VERSION
-		mkdir -p build
-		cd build
-		$CMAKE  -G "Ninja" \
-				-DCMAKE_MAKE_PROGRAM=$NINJA \
-				-DCMAKE_BUILD_PARALLEL_LEVEL=$((`nproc`-2)) \
-				-DCMAKE_BUILD_TYPE=Release \
-				-DCMAKE_C_COMPILER="${LLVM_DIR}/bin/clang" \
-				-DCMAKE_CXX_COMPILER="${LLVM_DIR}/bin/clang++" \
-				-DCMAKE_LINKER="${LLVM_DIR}/bin/clang" \
-				\
-				-DCMAKE_CXX_FLAGS="-stdlib=libc++  -I${LLVM_DIR}/include/c++/v1" \
-				\
-				-DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld -static -stdlib=libc++  -rtlib=compiler-rt -L${LLVM_DIR}/lib  -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libunwind.a -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libc++abi.a" \
-				-DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld -static -stdlib=libc++  -rtlib=compiler-rt -L${LLVM_DIR}/lib -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libunwind.a -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libc++abi.a" \
-				\
-				-DCMAKE_INSTALL_PREFIX=$LLVM_DIR \
-				-DBUILD_SHARED_LIBS=OFF \
-				-DLLVM_STATIC_LINK_CXX_STDLIB=ON \
-				-DLLVM_TARGETS_TO_BUILD="X86;WebAssembly" \
-				-DLLVM_BUILD_TESTS=OFF \
-				-DLLVM_INCLUDE_TESTS=OFF \
-				-DCLANG_ENABLE_ARCMT=OFF \
-				../llvm >> $outfile
-		$CMAKE --build . --target install >> $outfile
 		touch $LLVM_DIR/$LLVMFile.$WASABI_LLVM.flag
 	elif [ "$WASABI_LLVM" = "git" ]
 	then
