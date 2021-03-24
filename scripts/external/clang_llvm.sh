96f974e9b598a6603e9115a42f60eba90fe425fc
I056617 <ghislain.hude@sap.com>
2021-03-24 23:43:39 +0100

    [llvm compilation]
    Former-commit-id: fbf46853fe8580edb4516fd60facaaf7644a78a1 [formerly fbf46853fe8580edb4516fd60facaaf7644a78a1 [formerly c9685d0bcd8c257ddf080455d117d10e2c93a926]]
    Former-commit-id: 72c340fd36c8394ab19de019a45a5bd38aa69851
    Former-commit-id: 870485e23ffd16b2154f14a462f3a343771284a1

--- scripts/external/clang_llvm.sh
+++ scripts/external/clang_llvm.sh
@@ -21,12 +21,12 @@ then
 			-DCMAKE_MAKE_PROGRAM=$NINJA \
 			-DCMAKE_BUILD_PARALLEL_LEVEL=$((`nproc`-2)) \
 			-DCMAKE_BUILD_TYPE=Release \
-			-DCMAKE_C_COMPILER="${LLVM_DIR}/bin/clang" \
-			-DCMAKE_CXX_COMPILER="${LLVM_DIR}/bin/clang++" \
-			-DCMAKE_LINKER="${LLVM_DIR}/bin/clang" \
+	#		-DCMAKE_C_COMPILER="${LLVM_DIR}/bin/clang" \
+	#		-DCMAKE_CXX_COMPILER="${LLVM_DIR}/bin/clang++" \
+	#		-DCMAKE_LINKER="${LLVM_DIR}/bin/clang" \
 			\
-			-DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld -static -L${LLVM_DIR}/lib" \
-			-DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld -static -L${LLVM_DIR}/lib" \
+	#		-DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld -static -L${LLVM_DIR}/lib" \
+	#		-DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld -static -L${LLVM_DIR}/lib" \
 			\
 			-DCMAKE_INSTALL_PREFIX=$LLVM_DIR \
 			-DBUILD_SHARED_LIBS=OFF \
