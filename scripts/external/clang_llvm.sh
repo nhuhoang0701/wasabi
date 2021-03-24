f8fd1bbade8dbe13d50d26fd2cc1674e6038c344
I056617 <ghislain.hude@sap.com>
2021-03-24 23:06:32 +0100

    [llvm]try to remove runtime dependencies
    Former-commit-id: 80e7d6045b005caf3dc1b0685ed2b8c55ef3f959 [formerly 80e7d6045b005caf3dc1b0685ed2b8c55ef3f959 [formerly a856d15aec9e486b9ceef41e114dc29688ecb98d]]
    Former-commit-id: d09b812085190d4be95b2a6b3380798fe46712ee
    Former-commit-id: 82acdbf7d30b5a8c69ec1d5986f7a406fdf6c7e1

--- scripts/external/clang_llvm.sh
+++ scripts/external/clang_llvm.sh
@@ -32,11 +32,15 @@ then
 				-DCMAKE_MAKE_PROGRAM=$NINJA \
 				-DCMAKE_BUILD_PARALLEL_LEVEL=$((`nproc`-2)) \
 				-DCMAKE_BUILD_TYPE=Release \
-				-DCMAKE_C_COMPILER=gcc \
-				-DCMAKE_EXE_LINKER_FLAGS="-static" \
-				-DCMAKE_C_FLAGS="-static" \
-				-DCMAKE_CXX_COMPILER=g++ \
-				-DCMAKE_CXX_FLAGS="-static-libstdc++" \
+				-DCMAKE_C_COMPILER="${LLVM_DIR}/bin/clang" \
+				-DCMAKE_CXX_COMPILER="${LLVM_DIR}/bin/clang++" \
+				-DCMAKE_LINKER="${LLVM_DIR}/bin/clang" \
+				\
+				-DCMAKE_CXX_FLAGS="-stdlib=libc++  -I${LLVM_DIR}/include/c++/v1" \
+				\
+				-DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld -static -stdlib=libc++  -rtlib=compiler-rt -L${LLVM_DIR}/lib  -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libunwind.a -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libc++abi.a" \
+				-DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld -static -stdlib=libc++  -rtlib=compiler-rt -L${LLVM_DIR}/lib -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libunwind.a -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libc++abi.a" \
+				\
 				-DCMAKE_INSTALL_PREFIX=$LLVM_DIR \
 				-DBUILD_SHARED_LIBS=OFF \
 				-DLLVM_STATIC_LINK_CXX_STDLIB=ON \
