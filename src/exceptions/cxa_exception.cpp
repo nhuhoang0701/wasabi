e4b2f37f932b103e2fd2110bd999123a056a3963
I056617 <ghislain.hude@sap.com>
2021-04-02 12:31:36 +0200

    clean error message during eception
    Former-commit-id: c09d12ff4584406f6c421f2ea513ab7c96baa2bd [formerly c09d12ff4584406f6c421f2ea513ab7c96baa2bd [formerly bbef0d74fb67928d8a41ef15a0f60d51d2cdcc34]]
    Former-commit-id: 9ad91e188682f24816bf8a00f1711ab19b14c7cb
    Former-commit-id: bee27000cfa89805d2d7455ca0bdbaa31842ce16

--- src/exceptions/cxa_exception.cpp
+++ src/exceptions/cxa_exception.cpp
@@ -1,3 +1,4 @@
+#include <exception>
 #include <iostream>
 #include <stdexcept>
 #include <typeinfo>
@@ -10,40 +11,41 @@ extern "C"
 
 void *__cxa_allocate_exception(size_t thrown_size) throw()
 {
-	std::cerr << "WASABI:Error: Called unimplemented method: __cxa_allocate_exception" << std::endl;
-	return NULL;
+	void* ptr = malloc(thrown_size);
+	if(ptr == nullptr)
+		std::terminate();
+	//TODO: Need a logger to log error but not in the console
+	return ptr;
 }
 
 void __cxa_throw(void *thrown_object, std::type_info *tinfo, void (*dest)(void *))
 {
-	std::cerr << "WASABI:Error: Throwing exception!!!" << std::endl;
+	std::cerr << "WASABI:Error: Throwing exception. Exception's type info name: ";
 	if (tinfo && tinfo->name())
 	{
-		std::cerr << "WASABI:Exception's type info name: " << tinfo->name() << std::endl;
+		std::cerr << " '" << tinfo->name() << "'";
 	}
 	else
 	{
-		std::cerr << "WASABI:Exception's type: unknown" << std::endl;
+		std::cerr << " 'unknown'";
 	}
 
-	std::cerr << "WASABI:Attempting to read as std::exception... ";
 	std::exception* x = reinterpret_cast<std::exception*>(thrown_object); // this may crash
-	std::cerr << std::flush << "WASABI:Error message: \"" << x->what() << "\"" << std::endl;
+	std::cerr << std::flush << " message: '" << x->what() << "'" << std::endl;
 
-	std::cerr << "WASABI:Exiting program" << std::endl;
-	exit(-1);
+	std::terminate();
 }
 
 
 void* __cxa_begin_catch(void* unwind_arg) throw()
 {
-	std::cerr << "WASABI:Error: Called unimplemented method: __cxa_begin_catch" << std::endl;
+	std::cerr << "WASABI: NYI: __cxa_begin_catch" << std::endl;
 	return unwind_arg;
 }
 
 void __cxa_end_catch() throw()
 {
-	std::cerr << "WASABI:Error: Called unimplemented method: __cxa_end_catch" << std::endl;
+	std::cerr << "WASABI: NYI: __cxa_end_catch" << std::endl;
 }
 
 }
