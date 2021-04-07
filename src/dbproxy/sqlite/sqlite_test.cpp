2da873e072cd253e77777c9418faf982326ffc1d
I056617 <ghislain.hude@sap.com>
2021-04-07 15:28:01 +0200

    [dbproxy]fix leak in test
    Former-commit-id: 26da9475732b4184d1df3206ffdbd31aa66570ae

--- src/dbproxy/sqlite/sqlite_test.cpp
+++ src/dbproxy/sqlite/sqlite_test.cpp
@@ -10,6 +10,7 @@ int main()
 	
 	sqlite3* sqlite_db = nullptr;
 	int res = sqlite3_open_v2("./resources/sqlite/efashion/efashion.db", &sqlite_db, SQLITE_OPEN_READONLY, nullptr);
+	sqlite3_close(sqlite_db);
 	CPPUNIT_ASSERT_MSG(0==res,"sqlite3_open_v2 return an error.");
 	return TEST_HAVEERROR();
 }
