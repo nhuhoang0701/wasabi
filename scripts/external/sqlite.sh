2adc942c637403631d19c42af658c5c4717ecda5
I056617 <ghislain.hude@sap.com>
2021-04-20 13:50:04 +0200

    [sqlite/dbproxy] use sqlite lib and include from the external compiled/installed sqlite
    Former-commit-id: cf842fd3d146344b972b98d34aca86c72b344698

--- scripts/external/sqlite.sh
+++ scripts/external/sqlite.sh
@@ -9,10 +9,9 @@ then
 	git clone https://github.com/wapm-packages/sqlite.git  >> $outfile
 	cd sqlite
 	mkdir -p build_$WASABI_PLATFORM_TARGET
-	cd build_$WASABI_PLATFORM_TARGET
 
 	echo ------------- cmake ---------------
-	$CMAKE .. \
+	$CMAKE -B ./build_$WASABI_PLATFORM_TARGET . \
 		-G Ninja -DCMAKE_MAKE_PROGRAM=$NINJA \
 		-DCMAKE_TOOLCHAIN_FILE=$WASABI_ROOT_DIR/scripts/cmake/wasabi.cmake \
 		-DCMAKE_C_FLAGS=-fno-stack-protector \
@@ -20,7 +19,7 @@ then
 		-DCMAKE_INSTALL_PREFIX=$WASABI_INSTAL_DIR/sqlite >> $outfile
 
 	echo ------------- build ---------------
-	$CMAKE --build_$WASABI_PLATFORM_TARGET . >> $outfile
+	$CMAKE --build ./build_$WASABI_PLATFORM_TARGET --target install  >> $outfile
 	touch $WASABI_INSTAL_DIR/sqlite/sqlite.flag
 else
 	echo "already installed in '$WASABI_INSTAL_DIR/sqlite'"
