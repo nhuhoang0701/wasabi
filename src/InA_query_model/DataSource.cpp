25faaa426bf2ad8ce946dc86dd601e43de0f66be
I056617 <ghislain.hude@sap.com>
2021-04-21 17:35:47 +0200

    [InA query model] USe some hardcoded table and cnxstring
    Former-commit-id: fc7d8b081ca67ef4f2882c4fdd28e8dfd109cc40 [formerly fc7d8b081ca67ef4f2882c4fdd28e8dfd109cc40 [formerly ff09b0823326b244dd5fd157a819635b32471c4f]]
    Former-commit-id: d7ca66bff7e4aecc3c3a1aa4975f567ce6234897
    Former-commit-id: 488432c3b3390f6f31f580a9e81e580b733cea62

--- src/InA_query_model/DataSource.cpp
+++ src/InA_query_model/DataSource.cpp
@@ -1,6 +1,7 @@
 #include "DataSource.h"
 
 #include <exceptions/InA_Exception.h>
+#include <iostream>
 
 namespace ina::query_model
 {
@@ -41,6 +42,13 @@ namespace ina::query_model
 
 	const std::string&	DataSource::getObjectName() const
 	{
+		const static std::string table = "Agg_yr_qt_mt_mn_wk_rg_cy_sn_sr_qt_ma";
+		//TODO: Remove just to start dev.
+		if(m_objName.empty())
+		{
+			std::cerr << "Harcoded table will be used: " << table << std::endl;
+			return table;
+		}
 		return m_objName;
 	}
 
@@ -51,6 +59,13 @@ namespace ina::query_model
 
 	const std::string& DataSource::getPackageName() const
 	{
+		const static std::string cnxstrg =  "local:sqlite:efashion";
+		//TODO: Remove just to start dev.
+		if(m_packageName.empty())
+		{
+			std::cerr << "Harcoded connection string will be used: " << cnxstrg << std::endl;
+			return cnxstrg;
+		}
 		return m_packageName;
 	}
 
