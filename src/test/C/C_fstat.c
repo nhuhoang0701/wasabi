cf3d07e5877ebb22c6e6b41e7c972cb2dfbfac4d
I056617 <ghislain.hude@sap.com>
2021-04-22 22:23:23 +0200

    [WASI]fd_filestat_get can be call before read.
    Former-commit-id: 69281c06115a7d5c400e5b6d37b2bd3b60e8a637 [formerly 69281c06115a7d5c400e5b6d37b2bd3b60e8a637 [formerly 69281c06115a7d5c400e5b6d37b2bd3b60e8a637 [formerly 38c08688632878aa8aee926e505e3be7cdca1f6d]]]
    Former-commit-id: c4aae982a152ae1357c21cea8aaa47c02fd6aee3
    Former-commit-id: a17af60006e2b662d02eef7f8ff37a1cc8392d52
    Former-commit-id: 2b0f572940a7c8762ddceb7ca889ec843f4a8b22

--- src/test/C/C_fstat.c
+++ src/test/C/C_fstat.c
@@ -1,16 +1,8 @@
 #include <test_tools/TestAssert.h>
 
-#include <stdio.h>         // For printf
-#include <math.h>          // For pow, sqrt
-#include <string.h>        // For memcpy
-#include <stdlib.h>        // For malloc and co
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
-
-//TODO: Centralyse it
-#define EXPORT 
-
 
 int main()
 {
