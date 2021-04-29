ac22e3e26d799f78467b5cc68af638aecb3a62ee
I056617 <ghislain.hude@sap.com>
2021-04-30 00:35:34 +0200

    [InA_Interpreter]Fix bad file name
    Former-commit-id: acc80644f0253572bbec88f4d768475c69089081 [formerly acc80644f0253572bbec88f4d768475c69089081 [formerly d4f7fe1635832e38c2dfab00ecd9b45d6aefcd73]]
    Former-commit-id: 356d3274b64f5ab8b3b5d489b07746c447ef905a
    Former-commit-id: dc2559ce6ab0aa0daba1bcdf7785b71fd0174d2c

--- src/wasi_browser/Microcube_worker.js
+++ src/wasi_browser/Microcube_worker.js
@@ -35,100 +35,90 @@ onmessage = function(e) {
 		console.log(param);
 	}
 
-	switch(action){
-    case WorkerEvent.eLoad:
-		if(isLoaded == true){
-			console.error("Worker: Already loaded");
-			postMessage([ID, action, {error: "Already loaded"}]);
-			return;
-		}
+	try {
+		switch(action){
+		case WorkerEvent.eLoad:
+			if(isLoaded == true){
+				console.error("Worker: Already loaded");
+				postMessage([ID, action, {error: "Already loaded"}]);
+				return;
+			}
 
-		var importObject =
-		{
-			wasi_snapshot_preview1: WASI_API,
-			env: {},
-			js : {mem: new WebAssembly.Memory({initial: 2,maximum: 100})}
-		};
-		WebAssembly.instantiateStreaming(fetch("./InA_Interpreter.wasm"), importObject).then(module =>
-		{
-			console.log('Worker: Library well loaded');
-			moduleWASI = module;
-			setModuleInstance(module.instance);
-			
-			filesystem = ["/resources/response_getSerververInfo.json",
-						 "/resources/response_getResponse_Metadat_expand_cube_catalog.json",
-						 "/resources/sqlite/efashion/efashion.db"];
-			WASI_API.wasabi_initFS(param, filesystem).then(() => 
+			var importObject =
 			{
-				if(getModuleInstance()._initialize)
+				wasi_snapshot_preview1: WASI_API,
+				env: {},
+				js : {mem: new WebAssembly.Memory({initial: 2,maximum: 100})}
+			};
+			WebAssembly.instantiateStreaming(fetch("./InA_Interpreter.wasm"), importObject).then(module =>
+			{
+				console.log('Worker: Library well loaded');
+				moduleWASI = module;
+				setModuleInstance(module.instance);
+				
+				filesystem = ["/resources/response_getSerververInfo.json",
+							"/resources/response_getResponse_Metadata_expand_cube_catalog.json",
+							"/resources/sqlite/efashion/efashion.db"];
+				WASI_API.wasabi_initFS(param, filesystem).then(() => 
 				{
-					console.log('Worker: execute _initialize');
-					getModuleInstance()._initialize();
-				}
-				else if(getModuleInstance()._start)
-				{
-					console.log('Worker: execute _start');
-					getModuleInstance()._start();
-				}
-				else
-				{
-					console.log('Worker: no _start entry point');
-				}
-				postMessage([ID, WorkerEvent.eLoad, "{\"message\": \"Library well loaded\"}"]);
-				isLoaded = true;
+					if(getModuleInstance()._initialize)
+					{
+						console.log('Worker: execute _initialize');
+						getModuleInstance()._initialize();
+					}
+					else if(getModuleInstance()._start)
+					{
+						console.log('Worker: execute _start');
+						getModuleInstance()._start();
+					}
+					else
+					{
+						console.log('Worker: no _start entry point');
+					}
+					postMessage([ID, WorkerEvent.eLoad, "{\"message\": \"Library well loaded\"}"]);
+					isLoaded = true;
+				});
+			}).catch(error=>{
+				throw error;
 			});
-		}).catch(error=>{
-			console.log('Worker: Library not well loaded: ', error),
-			console.log("stack" + error.stack),
-			postMessage([ID, WorkerEvent.eLoad, error])
-		});
 
-		return;
-    case WorkerEvent.eGetServerInfo:
-		if(isLoaded == false){
-			console.error("Worker: Not loaded");
-			postMessage([ID, WorkerEvent.eGetServerInfo, {error: "Not loaded"}]);
 			return;
-		}
+		case WorkerEvent.eGetServerInfo:
+			if(isLoaded == false){
+				console.error("Worker: Not loaded");
+				postMessage([ID, WorkerEvent.eGetServerInfo, {error: "Not loaded"}]);
+				return;
+			}
 
-		try {
 			var wasmRes = moduleWASI.instance.exports.json_getServerInfo();
 			var res = convertWAsmStr2JSStr(wasmRes, moduleWASI.instance.exports.memory);
 
 			console.log([WorkerEvent.eGetServerInfo, 'Worker: GetServerInfo executed']);
 			postMessage([ID, WorkerEvent.eGetServerInfo, res]);
-		} catch(error) {
-			console.log('Worker: eGetServerInfo error: ', error);
-			console.log("stack" + error.stack),
-			postMessage([ID, WorkerEvent.eGetServerInfo, error]);
-		}
-		
-		return;
-    case WorkerEvent.eGetResponse:
-		if(isLoaded == false){
-			console.error("Worker: Not loaded");
-			postMessage([ID, WorkerEvent.eGetResponse, {error: "Not loaded"}]);
-			return;
-		}
-
-		try {
-			var queryJS = message[indexMsgParam];
 			
+			return;
+		case WorkerEvent.eGetResponse:
+			if(isLoaded == false){
+				console.error("Worker: Not loaded");
+				postMessage([ID, WorkerEvent.eGetResponse, {error: "Not loaded"}]);
+				return;
+			}
+
+			var queryJS = message[indexMsgParam];
 			var queryWAsm = convertJSStr2WAsm(queryJS, moduleWASI);
 			var wasmRes = moduleWASI.instance.exports.json_getResponse_json(queryWAsm);
 			moduleWASI.instance.exports.free(queryWAsm);
-			
 			var res = convertWAsmStr2JSStr(wasmRes, moduleWASI.instance.exports.memory);
 
 			console.log([WorkerEvent.eGetResponse, 'Worker: GetServerInfo executed']);
 			postMessage([ID, WorkerEvent.eGetResponse, res]);
-		} catch(error) {
-			console.log('Worker: eGetResponse error: ', error);
-			console.log("stack" + error.stack),
-			postMessage([ID, WorkerEvent.eGetResponse, error]);
+			
+			return;
 		}
-		
-		return;
+	} catch(error) {
+		console.log('Worker: error: ', error);
+		console.log("stack: ", error.stack),
+		postMessage([ID, action, {"message": error+""}]);
 	}
 	
 	postMessage([ID, action, new Error('Unknow action:' + action)]);
