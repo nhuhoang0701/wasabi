3b094af504d01aad11412325a198095d8e76b416
Prasanth MOHANACHANDRAN <p.mohanachandran@sap.com>
2021-05-05 17:34:21 +0200

    Functions (#78)
    * Functions
    
    * Modified file
    
    * new changes for Formulas
    
    Former-commit-id: 7c3ed771494332bc9f64e27b3c819ce1e566171c [formerly 7c3ed771494332bc9f64e27b3c819ce1e566171c [formerly 7c3ed771494332bc9f64e27b3c819ce1e566171c [formerly 571aab282c0cac35997d0e90e2560e06e0cef517]]]
    Former-commit-id: de4af2467a71593e36285b89b1f82016033cf5ff
    Former-commit-id: 799b69544c439d5c4e4425a6132f49108a9768bc
    Former-commit-id: 84459a8ffa72ec909e5b9b0b9466a38f183166c0

---
+++ src/InA_query_model/Parameter.cpp
@@ -0,0 +1,37 @@
+#include "Parameter.h"
+#include "Function.h"
+#include <cstddef>
+#include <exceptions/InA_Exception.h>
+
+namespace ina::query_model
+{
+    Parameter::Parameter(Parameter::eType type, const std::string& value):
+    m_type(type),
+    m_value(value),
+    m_function(nullptr)
+    {
+
+    }
+    
+    Parameter::~Parameter()
+    {        
+    }
+    
+    Parameter::eType Parameter::getType() const
+    {
+        return m_type;
+    }
+
+    const std::string & Parameter::getValue() const
+    {
+        return m_value;
+    }
+
+    const Function& Parameter::getFunction() const
+    {
+        if(m_function == nullptr)
+            throw TRACED_InA_EXCEPTION("invalid call to getFunction");
+        return *m_function;
+    }
+
+}
