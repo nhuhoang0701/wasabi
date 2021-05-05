7ca647ec1a09ca3a0c9a3194e5f35537972361e6
I056617 <ghislain.hude@sap.com>
2021-05-05 17:41:19 +0200

    [ina interpreter] add some ina errors [ina metadta] WIP
    Former-commit-id: c9a79cb9c5bce6f88b3d796551076f902686982c [formerly c9a79cb9c5bce6f88b3d796551076f902686982c [formerly c9a79cb9c5bce6f88b3d796551076f902686982c [formerly c9a79cb9c5bce6f88b3d796551076f902686982c [formerly 56955bbea560975f1602fad8e93451d20beb427a]]]]
    Former-commit-id: 705fc2b68558c15a6df0dc1a87131213948441ac
    Former-commit-id: ae7257fd587024c3174cb87ca1f66fbb97eec8fa
    Former-commit-id: 87a01de7d41d6fefd6c50d9b38b99aa90a7a4dc8
    Former-commit-id: 8e781c21cd658ff52917d4d0232b96ccb9380077

--- src/InA_metadata/Dimension.h
+++ src/InA_metadata/Dimension.h
@@ -15,11 +15,12 @@ namespace ina::metadata
 
 	void write(const Dimension& obj, JSONWriter& jsonNode);
 
+	enum class eAxe {Uninit=0, Rows, Columns};
+	enum class eDimType {Uninit=0, MeasuresDimension = 2, Dimension = 3};
+
 	class Dimension
 	{
 		public:
-			enum class eAxe {Uninit=0, Rows, Columns};
-			enum class eDimType {Uninit=0, MeasuresDimension = 2, Dimension = 3};
 
 			Dimension() = default;
 			Dimension(const std::string & name, eAxe axename);
@@ -31,20 +32,19 @@ namespace ina::metadata
 			const std::string&  getNameExternal() const;
 			const std::string&  getDescription() const;
 
-			eDimType             getDimensionType() const;
-			bool                 isDimensionGroup();
-			bool                 isModeled() const;
-			bool                 canBeAggregated() const;
-			const std::string&   getAxisDefault() const;
+			virtual eDimType             getDimensionType() const;
+			virtual bool                 isDimensionGroup();
+			virtual bool                 isModeled() const;
+			virtual bool                 canBeAggregated() const;
+			virtual const std::string&   getAxisDefault() const;
 
 			void                          addAttribute(const Attribute &att);
 			const std::vector<Attribute>& getAttributes() const;
 			const Attribute&              getKeyAttribute() const;
 			bool                          haveTextAttribute() const;
 			const Attribute&              getTextAttribute() const;
-		
 
-			size_t              getCardinality() const;
+			size_t                        getCardinality() const;
 
 			void                          addMember(const Member & member);
 			const std::vector<Member>&    getMembers() const;
@@ -55,7 +55,22 @@ namespace ina::metadata
 
 			std::vector<Member>     _members;
 			std::vector<Attribute>  _attributes;
+			int8_t                  _keyAttributes = -1; // index in _attributes
+			int8_t                  _textAttributes = -1;// index in _attributes
 
 			friend void write(const Dimension& obj, JSONWriter& jsonNode);
+	};
+
+	class DimensionMeasures : public Dimension
+	{
+		public:
+			DimensionMeasures() = default;
+			DimensionMeasures(const std::string & name, eAxe axename) = default;
+
+			virtual eDimType             getDimensionType() const;
+			virtual bool                 isDimensionGroup();
+			virtual bool                 isModeled() const;
+			virtual bool                 canBeAggregated() const;
+			virtual const std::string&   getAxisDefault() const;
 	};
 }
