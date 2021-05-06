b5dc0333d789c5105a5879c1d6add5f279dba338
I056617 <ghislain.hude@sap.com>
2021-05-06 20:19:37 +0200

    [ina metadata] WIP
    Former-commit-id: 865363f292872ae9ecdf3924f02c845dc0bc0e55

---
+++ src/InA_metadata/Attribute.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+
+class JSONWriter;        // From <json/jsonWriter.h>
+
+namespace ina::metadata
+{
+	class Dimension;
+	class Attribut;
+
+	void write(const Attribut& obj, JSONWriter& jsonNode);
+
+	enum class eDatatype {Uninit=-1, String = 2, Number = 3};
+	enum class ColumnType {Uninit=-1};
+
+	class Attribut
+	{
+		public:
+
+			Attribut(const Dimension& dimension);
+			
+			const std::string&  getName() const;
+			const std::string&  getUniqueName() const;
+			const std::string&  getNameExternal() const;
+			const std::string&  getDescription() const;
+
+			eDatatype             getDatatype() const;
+			virtual bool                 isDimensionGroup();
+			virtual bool                 isModeled() const;
+			virtual bool                 canBeAggregated() const;
+			virtual const std::string&   getAxisDefault() const;
+
+			size_t                        getCardinality() const;
+
+		private:
+			std::string _name; 
+
+			friend void write(const Attribut& obj, JSONWriter& jsonNode);
+	};
+}
