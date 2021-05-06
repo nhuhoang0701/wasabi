442af19e71a04cfa78dd59722203ef2d9a35ef0c
I056617 <ghislain.hude@sap.com>
2021-05-06 23:37:43 +0200

    [cs test]prefix JS global function
    Former-commit-id: eccc879820b7f0fda144bf0b55a39c3471632f85

--- src/wasi_browser/CS_test.js
+++ src/wasi_browser/CS_test.js
@@ -4,41 +4,41 @@ const networkLayer = "JDBC";
 const dbEngine = "SQLite for test - efashion";
 const xmlConnectionDesc = "<?xml version=\"1.0\"?><ConnectionDefinition><Parameter Category=\"IDENTIFICATION\" ID=\"NETWORKLAYER\" Type=\"String\">JDBC</Parameter><Parameter Category=\"IDENTIFICATION\" ID=\"DBMS\" Type=\"String\">SQLite for test - memory</Parameter><Parameter Category=\"IDENTIFICATION\" ID=\"CONNECTIVITY_TYPE\" Type=\"String\">Relational</Parameter><Parameter Category=\"CONFIGURATION\" ID=\"MAX_PARALLEL_QUERIES\" Type=\"Integer\">4</Parameter><Parameter Category=\"CREDENTIALS\" ID=\"PASSWORD\" Type=\"String\"></Parameter><Parameter Category=\"CREDENTIALS\" ID=\"DATASOURCE\" Type=\"String\"></Parameter><Parameter Category=\"CREDENTIALS\" ID=\"AUTHENTICATION_MODE\" Type=\"Enum\">ConfiguredIdentity</Parameter><Parameter Category=\"CREDENTIALS\" ID=\"USER\" Type=\"String\"></Parameter></ConnectionDefinition>";
 
-function log(msg) {
+function cs_log(msg) {
     if(typeof document !== 'undefined') {
         let msgCR = msg + '\n';
         document.getElementById('log').innerHTML += msgCR.replace(/\n( *)/g, function (match, p1) {
             return '<br>' + '&nbsp;'.repeat(p1.length);
         });
     }
-    console.log(msg);
+    console.cs_log(msg);
 }
 
 function _startCSTest() {
-    log("******************************************");
-    log("Start CS test.");
-    log("******************************************");
+    cs_log("******************************************");
+    cs_log("Start CS test.");
+    cs_log("******************************************");
     
     let cs = new ProtoCS.ConnectionServer();
     cs.configure(endpoint);
     cs_createJob(cs).then((job) => {
         let sql = document.querySelector('#enter').value;
-        log("Execute SQL:" + sql);
+        cs_log("Execute SQL:" + sql);
         job.Execute(sql, undefined, undefined).then((result) => {
             result.setSliceSize(500);
             this.cs_fetch(result);
         }).catch(error=>{
-            log("ERROR: id:" + error.id);
-            log("       type:" + error.type);
-            log("       message:" + error.message);
+            cs_log("ERROR: id:" + error.id);
+            cs_log("       type:" + error.type);
+            cs_log("       message:" + error.message);
         });                            
     })
 }
 
 function cs_createJob(cs){
-    log("******************************************");
-    log("Creating Job.");
-    log("******************************************");
+    cs_log("******************************************");
+    cs_log("Creating Job.");
+    cs_log("******************************************");
     return new Promise((resolve, reject) => {
         cs.JobProfile(networkLayer, dbEngine).then((jd) => {
             jd.Properties().then((jps) => {
@@ -55,23 +55,23 @@ function cs_createJob(cs){
 }
 
 async function cs_fetch(result) {
-    log("******************************************");
-    log("Fetching Columns.");
-    log("******************************************");
+    cs_log("******************************************");
+    cs_log("Fetching Columns.");
+    cs_log("******************************************");
     for(let i = 0; i < result.Descriptions().length; i++) {
         var cd = result.Descriptions()[i];
-        log(cd.name + " " + cd.typeName);
+        cs_log(cd.name + " " + cd.typeName);
     }
-    log("******************************************");
-    log("Fetching Results.");
-    log("******************************************");
+    cs_log("******************************************");
+    cs_log("Fetching Results.");
+    cs_log("******************************************");
 
     for await (let record of result.Records().asyncIterator()) {
         let line="";
         for(let i = 0; i < record.length; i++) {
             line += record[i].getValueAsString()+"\t";
         }
-        log(line);
+        cs_log(line);
     }
-    log("All fetched!");
+    cs_log("All fetched!");
 }
