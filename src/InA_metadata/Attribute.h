87f1bc0244725b897127f6d8ad99a72ccf1bd089
I056617 <ghislain.hude@sap.com>
2021-05-08 18:43:19 +0200

    [InA model] First model version with some hardcoded field (like LDC/LUC)
    Former-commit-id: e304cfa5219368c06be7f9c23e83cf05201e08ea [formerly e304cfa5219368c06be7f9c23e83cf05201e08ea [formerly a6291cca35da5ab63f030bbc5a037ecd5e63d1c8]]
    Former-commit-id: fa8b520167a27999ce9adccd6968152261eaa977
    Former-commit-id: 77fefe222242fbd84b7560e2dfb5bda2ef4796fe

--- src/InA_metadata/Attribute.h
+++ src/InA_metadata/Attribute.h
@@ -12,7 +12,8 @@ namespace ina::metadata
 
 	void write(const Attribute& obj, JSONWriter& jsonNode);
 
-	enum class eDatatype {Uninit=-1, String = 2, Number = 3};
+	enum class eDatatype {Uninit=-1, String = 3, Long = 1};
+	std::string toString(eDatatype);
 	enum class eColumnType {Uninit=-1,NumericMeasure = 73,NoNumericMeasure = 83,NoMeasure = 0};
 
 	class Attribute
@@ -25,21 +26,30 @@ namespace ina::metadata
 
 			const std::string&  getName() const;
 			const std::string&  getUniqueName() const;
-			const std::string&  getNameExternal() const;
-			const std::string&  getDescription() const;
+			const std::string   getNameExternal() const;
+			const std::string   getDescription() const;
 
 			const std::string&   getAlias() const;
+
 			eDatatype            getDatatype() const;
+			const std::string&   getSQLDataType() const;
+			
+			bool                 isKey() const;
+			const std::string    getPresentationType() const;
+
 			eColumnType          getColumnType() const;
+
 			bool                 isDisplayed() const;
 			bool                 isFilterable() const;
-			bool                 isFlatKey() const;
+
 			bool                 isNotNull() const;
 
 			std::uint32_t        getCardinality() const;
 
 		private:
 			std::string      m_name; 
+			std::string      m_description;
+
 			const Dimension* m_dimension = nullptr;
 
 			friend void write(const Attribute& obj, JSONWriter& jsonNode);
