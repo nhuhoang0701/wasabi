e9c4dd2a58c49ed947b895decc0b74308b4f02f4
I056617 <ghislain.hude@sap.com>
2021-05-09 15:54:53 +0200

    use LLVM_DIR var. as possible
    Former-commit-id: a3653dbaec44178c96a04b09d24618465236d97a [formerly a3653dbaec44178c96a04b09d24618465236d97a [formerly 649a2b42c73989fecd9e2a8317e870d59489fc15]]
    Former-commit-id: 2ccb08926d8a5529ceb4e174920ee701b7861a8a
    Former-commit-id: 85befe87ff30067a5195d60eb0e5d30331f70eeb

--- scripts/external/wasmsdk.sh
+++ scripts/external/wasmsdk.sh
@@ -10,9 +10,9 @@ then
 	cd wasmsdk4compile
 	git clone https://github.com/WebAssembly/wasi-libc.git  >> $outfile
 	cd wasi-libc
-	make WASM_CC=$WASABI_EXTERNAL_DIR/llvm/bin/clang WASM_AR=$WASABI_EXTERNAL_DIR/llvm/bin/llvm-ar WASM_NM=$WASABI_EXTERNAL_DIR/llvm/bin/llvm-nm INSTALL_DIR=$WASABI_INSTALL_DIR/wasi_libc WASM_CFLAGS=-g -j$((`nproc` -2))
+	make WASM_CC=$LLVM_DIR/bin/clang WASM_AR=$LLVM_DIR/bin/llvm-ar WASM_NM=$LLVM_DIR/bin/llvm-nm INSTALL_DIR=$WASABI_INSTALL_DIR/wasi_libc WASM_CFLAGS=-g -j$((`nproc` -2))
 	echo ------------ install --------------
-	make WASM_CC=$WASABI_EXTERNAL_DIR/llvm/bin/clang WASM_AR=$WASABI_EXTERNAL_DIR/llvm/bin/llvm-ar WASM_NM=$WASABI_EXTERNAL_DIR/llvm/bin/llvm-nm INSTALL_DIR=$WASABI_INSTALL_DIR/wasi_libc WASM_CFLAGS=-g -j$((`nproc` -2)) install
+	make WASM_CC=$LLVM_DIR/bin/clang WASM_AR=$LLVM_DIR/bin/llvm-ar WASM_NM=$LLVM_DIR/bin/llvm-nm INSTALL_DIR=$WASABI_INSTALL_DIR/wasi_libc WASM_CFLAGS=-g -j$((`nproc` -2)) install
 	touch $WASMSDK_DIR/wasmsdk.flag
 else
 	echo "already installed in '$WASMSDK_DIR'"
