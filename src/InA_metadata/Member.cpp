e6f35a274139a29a4907efe9106cbb90b984fdb3
I056617 <ghislain.hude@sap.com>
2021-05-09 15:48:16 +0200

    [ina metadata] Move members creation of the Dimension class
    Former-commit-id: cc87fc4521b9abb2ecf465176a9ec8e423bc81f8 [formerly cc87fc4521b9abb2ecf465176a9ec8e423bc81f8 [formerly cc87fc4521b9abb2ecf465176a9ec8e423bc81f8 [formerly cc87fc4521b9abb2ecf465176a9ec8e423bc81f8 [formerly 43d4cb09181d5f234cbc4dcacfd03f6e05dffb8e]]]]
    Former-commit-id: 6b782997c3a3bb8f6885b2562fffebe46b752fe4
    Former-commit-id: 3f08649afc14683156ca6829528220194969d0d1
    Former-commit-id: 590a27967a7243352b76ec744353527045396124
    Former-commit-id: 7b2d5c79ce8d36957d19db7dc5ad63939886c429

--- src/InA_metadata/Member.cpp
+++ src/InA_metadata/Member.cpp
@@ -3,12 +3,26 @@
 
 namespace ina::metadata
 {
-    Member::Member(const Dimension& dimension,
-			const std::pair<std::string, std::string>& uniqueName, 
-			const std::pair<std::string, std::string>& name, 
-			const std::pair<std::string, std::string>& description)
-            : m_uniquename(uniqueName), m_name(name), m_description(description)
+    Member::Member(const Dimension& dimension, const std::string& uniqueName, const std::string& name, const std::string& description)
     {
+        m_uniquename = {"[Measures].[Measures]", uniqueName};
+        m_name = {"[Measures].[Name]", name};
+        m_description ={"[Measures].[Description]", description};
+    }
+
+    Member::Member(const Member& other)
+    {
+        m_uniquename = other.m_uniquename;
+        m_name = other.m_name;
+        m_description = other.m_description;
+    }
+    Member& Member::operator=(const Member& other)
+    {
+        m_uniquename = other.m_uniquename;
+        m_name = other.m_name;
+        m_description = other.m_description;
+
+        return *this;
     }
 
     const std::pair<std::string, std::string>& Member::getName() const
