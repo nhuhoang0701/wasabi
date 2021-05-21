9d83c23289e5f8dc4b2397c0af3dee151399b692
Christophe LAM WOON SIN <christophe.lam.woon.sin@sap.com>
2021-05-21 22:45:12 +0200

    [query_generator] complex selection fix (#89)
    * Filter: update to be closer to INA
    
    * Taking comments from Ghislain into account
    
    * Changing to a simpler model
    
    * Refactoring QueryFilterComparison into Element (as in INA)
    
    * Code Review
    
    * Selection parsing fails if there is no Operator
    
    * Taking comments from Ghislain into account
    
    * [UnitTests] fix
    
    Co-authored-by: i051517 <christophe.lam.woon.sin on.sinn@sap.com>
    Former-commit-id: ea04ac16154cdc8c734c8aeed06aa46177534d71 [formerly ea04ac16154cdc8c734c8aeed06aa46177534d71 [formerly ea04ac16154cdc8c734c8aeed06aa46177534d71 [formerly 6997df44095d07938610347bfd07143c58da1c1c]]]
    Former-commit-id: f7e53cfcfc4db04b6d0eec189de73cd4d0415632
    Former-commit-id: 144b607cdecc61c00a8b411cb5ccf2a85b53b1df
    Former-commit-id: e0f230ef5d5f73acacd37be2ab9ee2a586956321

--- src/InA_query_model/Selection/SelectionReader.cpp
+++ src/InA_query_model/Selection/SelectionReader.cpp
@@ -1,24 +1,24 @@
 #include "InA_query_model/Selection/SelectionElement.h"
 #include "Selection.h"
-
 #include <json/jsonReader.h>    // For JSONGenericObject
 
 namespace ina::query_model
 {
 	void read(Selection& selection, const JSONGenericObject& selectionNode)
     {       
-        SelectionElement selectionOperator;
-        JSONGenericObject operatorNode;
+        SelectionElement selectionElement;
+        JSONGenericObject selectionElementNode;
         if(selectionNode.getObject("Operator"))
         {
-            operatorNode = selectionNode.getObject("Operator");
-            selectionOperator = SelectionElement(SelectionElement::Type::Operator);
-        } else if (selectionNode.getObject("SetOperand"))
+            selectionElementNode = selectionNode.getObject("Operator");
+            selectionElement = SelectionElement(SelectionElement::Type::Operator);
+        } 
+        else if (selectionNode.getObject("SetOperand"))
         {
-            operatorNode = selectionNode.getObject("SetOperand");
-            selectionOperator = SelectionElement(SelectionElement::Type::SetOperand);
+            selectionElementNode = selectionNode.getObject("SetOperand");
+            selectionElement = SelectionElement(SelectionElement::Type::SetOperand);
         }
-        read(selectionOperator, operatorNode);
-        selection.setOperator(selectionOperator);
+        read(selectionElement, selectionElementNode);
+        selection.setOperator(selectionElement);
     }
 }
