0ab60ca4d3a8aaa4515e5cab29eb2a98d7d69c56
I056617 <ghislain.hude@sap.com>
2021-05-25 15:20:29 +0200

    [ina query model] Separate Selection Element and Elements reading
    Former-commit-id: 75ed817731a198f1142f475c3234a955f8a82f07 [formerly 75ed817731a198f1142f475c3234a955f8a82f07 [formerly 055ec7d547d9c88cc8d306b593203c5496d6089a]] [formerly 75ed817731a198f1142f475c3234a955f8a82f07 [formerly 75ed817731a198f1142f475c3234a955f8a82f07 [formerly 055ec7d547d9c88cc8d306b593203c5496d6089a]] [formerly 75ed817731a198f1142f475c3234a955f8a82f07 [formerly 055ec7d547d9c88cc8d306b593203c5496d6089a] [formerly 055ec7d547d9c88cc8d306b593203c5496d6089a [formerly 13d03bac974c469b126d5ececcd941ff8a9d5b4f]]]]
    Former-commit-id: 3756650eb60be9130a974acdef81faa69781ac9e
    Former-commit-id: f384b0c0b4e73f6809b8d3bb814c341856a455c1
    Former-commit-id: f6d722bcb5728f4baa3e7c9b5d0941dd03c7877e
    Former-commit-id: 5eafb4c7c448b638c1780cc7a367a01bc4f16110

--- src/InA_query_model/Selection/SelectionElement.cpp
+++ src/InA_query_model/Selection/SelectionElement.cpp
@@ -1,7 +1,33 @@
 #include "SelectionElement.h"
+#include <stdexcept>
 
 namespace ina::query_model 
 {
+    LogicalOperator getLogicalOperator(const std::string& str)
+    {
+        if (str == "And")
+            return LogicalOperator::And;
+        else if (str == "Not")
+            return LogicalOperator::Not;
+        else if (str == "Or")
+            return LogicalOperator::Or;
+        
+        throw std::runtime_error("InA Selection Operator: unknow: '"+str+"'" );
+    }
+    
+    std::string toString(const LogicalOperator logicalOperator) 
+    {
+        if (logicalOperator == LogicalOperator::And)
+            return "AND";
+        else if (logicalOperator == LogicalOperator::Not)
+            return "NOT";
+        else if (logicalOperator == LogicalOperator::Or)
+            return "OR";
+        else if (logicalOperator == LogicalOperator::Undefined)  
+            throw std::runtime_error("InA Selection Operator: 'Undefined' enum value" );;
+
+        throw std::runtime_error("InA Selection Operator: unknow: 'LogicalOperator'" );
+    }
 
 	SelectionElement::SelectionElement(Type type) 
     {
