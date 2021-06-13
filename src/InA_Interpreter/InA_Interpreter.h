3624e2e324fcaace8262155fd03d949d36b944dc
I056617 <ghislain.hude@sap.com>
2021-06-13 17:49:44 +0200

    WAsm Asyncify implementation (first one)
    Former-commit-id: fa22dd52c443867b22068b1ff1359b8a242952c6 [formerly fa22dd52c443867b22068b1ff1359b8a242952c6 [formerly e547b89b71b6babaad3285952d66929be5b68bad]]
    Former-commit-id: 68be2ab792a2fc37c8622d133c920812081a86fb
    Former-commit-id: a343d3b690461b629411ac10e48ee9ab608e41b9

--- src/InA_Interpreter/InA_Interpreter.h
+++ src/InA_Interpreter/InA_Interpreter.h
@@ -1,13 +1,51 @@
+//WARNINGS: 
+//	 Not threat safe: so calls all theses methods in the same theard or at least in a sequantial way.
 
-// Don't delete the returned char *
-// Not threat safe: so calls all theses methods in the same theard or at least in a sequantial way.
-#include <stdint.h>
+#include <stdint.h>   // For int32_t
 
-// these function will call this methoed to provide the resylt.
-// void ina_callback_response(int32_t ID, const char* action, const char* response)
+enum eRequestType
+{
+    eUndefined = -1,
+    eLoad = 0,
+    eGetServerInfo = 1,
+    eGetResponse = 2,
+    eSubmitCube = 3
+};
+static const char* toString(eRequestType type)
+{
+	switch (type)
+	{
+	case eLoad:
+		return "Load";
+	case eGetServerInfo:
+		return "GetServerInfo";
+	case eGetResponse:
+		return "GetResponse";
+	case eSubmitCube:
+		return "SubmitCube";
+	case eUndefined:
+		return "Undefined";
+	default:
+	    return nullptr;
+	}
+}
 
-extern "C" void void_getServerInfo_int32(int32_t ID);
-extern "C" void void_getResponse_int32_json(int32_t ID, const char* InA);
+/**
+ * @name doIt , the result will be provide throw the call bacl @ina_callback_response
+ *
+ * @param ID Identifier the request (will be propagated to the ina_callback_response)
+ * @param type Type of the request (will be propagated to the ina_callback_response)
+ * @param inaRequest The request , nullptr for GetServerInfo
+ *
+ */
+extern "C" void doIt(int32_t ID, eRequestType type, const char* inaRequest);
 
-// Should be define in webassembly side or in JavaScrpt side to received the InA response
-extern "C" void ina_callback_response(int32_t ID, const char* action, const char* response);
+/**
+ * @name ina_callback_response , callback which provide the result of doIt function
+ *
+ * @param ID Identifier the request (will be propagated to the ina_callback_response)
+ * @param type Type of the request (will be propagated to the ina_callback_response)
+ * @param inaRequest The request , nullptr for GetServerInfo
+ *
+ */
+extern "C" void ina_callback_response(int32_t ID, eRequestType type, const char* inaResponse);
