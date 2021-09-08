6f082b4d4a88fd82fcc088a28738a4aaefbb9001
Ghislain HUDE <ghislain.hude@sap.com>
2021-09-08 13:55:53 +0200

    CMake: replace some WASABI_BUILD_TYPE by CMAKE_BUILD_TYPE Factoryze some variables

--- scripts/cmake/wasabi.cmake
+++ scripts/cmake/wasabi.cmake
@@ -31,6 +31,9 @@ elseif (DEFINED ENV{WASABI_PLATFORM_TARGET})
 else()
 	message(FATAL_ERROR "Missing 'WASABI_PLATFORM_TARGET' definition")
 endif()
+if(NOT ${WASABI_PLATFORM_TARGET} MATCHES "^(wasm|linux)$")
+	message(FATAL_ERROR "${WASABI_PLATFORM_TARGET} should have 'wasm'/'linux' only, WASABI_PLATFORM_TARGET='${WASABI_PLATFORM_TARGET}'")
+endif()
 
 if(DEFINED WASMTIME)
 	message(STATUS "- WASMTIME='${WASMTIME}'")
@@ -77,68 +80,9 @@ else()
 endif()
 
 ###########################################
-# tools chain / xcompilation
-if ("${WASABI_PLATFORM_TARGET}" STREQUAL "wasm")
-	include(${CMAKE_CURRENT_LIST_DIR}/wasabiVar.cmake)
+# tools chain / xcompilation / compiler / linker
+include(${CMAKE_CURRENT_LIST_DIR}/wasabiVar.cmake)
 
-	set(CMAKE_CROSSCOMPILING_EMULATOR "${WASMTIME}")
-
-	set(CMAKE_SYSROOT ${SYSROOT_WASI_DIR})
-
-	if (NOT "${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-		set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
-	endif ()
-
-elseif ("${WASABI_PLATFORM_TARGET}" STREQUAL "linux")
-	set(CMAKE_C_COMPILER ${LLVM_DIR}/bin/clang)
-	set(CMAKE_CXX_COMPILER ${LLVM_DIR}/bin/clang++)
-	set(CMAKE_LINKER   ${LLVM_DIR}/bin/clang)
-	set(CMAKE_AR ${LLVM_DIR}/bin/llvm-ar)
-	set(CMAKE_RANLIB ${LLVM_DIR}/bin/llvm-ranlib)
-	set(CMAKE_SPLIT ${LLVM_DIR}/bin/llvm-split)
-	set(CMAKE_C_COMPILER_AR "${CMAKE_AR}")
-	set(CMAKE_CXX_COMPILER_AR "${CMAKE_AR}")
-	set(CMAKE_C_COMPILER_RANLIB "${CMAKE_RANLIB}")
-	set(CMAKE_CXX_COMPILER_RANLIB "${CMAKE_RANLIB}")
-
-	set(CMAKE_EXE_LINKER_FLAGS    "-fuse-ld=lld")
-	set(CMAKE_SHARED_LINKER_FLAGS    "-fuse-ld=lld")
-
-	#To use llvm libc++
-	#set(CMAKE_CXX_FLAGS           "${CMAKE_CXX_FLAGS} -stdlib=libc++  -I${LLVM_DIR}/include/c++/v1")
-	#set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -static-libstdc++ -rtlib=compiler-rt -stdlib=libc++ -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libunwind.a -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libc++abi.a")
-	#set(CMAKE_SHARED_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -static-libstdc++ -rtlib=compiler-rt -stdlib=libc++ -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libunwind.a -L${LLVM_DIR}/lib ${LLVM_DIR}/lib/libc++abi.a")
-
-	set(CMAKE_CXX_FLAGS           "${CMAKE_CXX_FLAGS} -gsplit-dwarf")
-	set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -gsplit-dwarf")
-	set(CMAKE_SHARED_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -gsplit-dwarf")
-
-if ("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-	set(CMAKE_CXX_FLAGS           "${CMAKE_CXX_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
-	set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
-	set(CMAKE_SHARED_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
-
-	set(CMAKE_CXX_FLAGS           "${CMAKE_CXX_FLAGS} -fsanitize=address")
-	set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address")
-	set(CMAKE_SHARED_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address")
-#	set(CMAKE_CXX_FLAGS           "${CMAKE_CXX_FLAGS} -fsanitize=memory")
-#	set(CMAKE_SHARED_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=memory")
-#	set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=memory")
-endif()
-
-#TODO: use musl as static libc
-#	set(CLANG_TARGET_TRIPLE x86_64-linux-musl)
-#	message(STATUS "- CLANG_TARGET_TRIPLE='${CLANG_TARGET_TRIPLE}'")
-
-#	set(CMAKE_SYSROOT ${SYSROOT_LINUX_DIR})
-
-#	set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -Wl,-rpath,${LLVM_DIR}/lib")
-#	set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-rpath,${LLVM_DIR}/lib")
-#	set(CMAKE_STATIC_LINKER_FLAGS "")
-#	set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -Wl,-rpath,${LLVM_DIR}/lib -Xlinker --allow-multiple-definition")
-else ()
-	message(FATAL_ERROR "${WASABI_PLATFORM_TARGET} should have 'wasm'/'linux' only, WASABI_PLATFORM_TARGET='${WASABI_PLATFORM_TARGET}'")
-endif ()
 
 ###########################################
 # compilation option
